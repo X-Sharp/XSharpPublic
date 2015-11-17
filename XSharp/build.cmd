b031156c40a06ebe0c17b606c219d710cf15fa0d
Robert van der Hulst <robert@heliks.nl>
2015-11-17 10:37:19 +0100

    Added our own build folder, and rolled back change to target files  in Roslyn folder Also added separate cmd files for build, clean and rebuild

--- XSharp/build.cmd
+++ XSharp/build.cmd
@@ -1,6 +1,9 @@
 @echo off
 SET Version=0.1.0
 Echo Building Compiler version %Version%
-Pause
-msbuild Compiler.sln /fl1 /p:Configuration=Debug	/p:RoslynSemanticVersion=%Version% /t:Rebuild /property:OfficialBuild=true /m /v:m 
-msbuild Compiler.sln /fl2 /p:Configuration=Release	/p:RoslynSemanticVersion=%Version% /t:Rebuild /property:OfficialBuild=true /m /v:m 
+msbuild Compiler.sln /fl1 /p:Configuration=Debug	/p:RoslynSemanticVersion=%Version% /t:Build /property:OfficialBuild=true /m /v:m /nologo
+msbuild Compiler.sln /fl2 /p:Configuration=Release	/p:RoslynSemanticVersion=%Version% /t:Build /property:OfficialBuild=true /m /v:m /nologo
+if exist build-debug.log del build-debug.log
+if exist build-release.log del build-release.log
+rename msbuild1.log build-debug.log
+rename msbuild2.log build-release.log

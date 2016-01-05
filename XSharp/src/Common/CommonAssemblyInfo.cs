548ff119464f75fda13f073c273bf64f1b112173
Robert van der Hulst <robert@heliks.nl>
2016-01-05 18:56:36 +0100

    Added  common files for assembly information and versioning

---
+++ XSharp/src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif	
+[assembly: AssemblyProduct("XSharp")]
+[assembly: AssemblyCompany("XSharp BV")]
+[assembly: AssemblyCopyright("Copyright ©  2015-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.6.0")]
+[assembly: AssemblyFileVersion("0.1.6.0")]
---
+++ XSharp/src/Common/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XSharp
+{
+    public static class Constants
+    {
+        public const string Company = "XSharpBV";
+        public const string Product = "XSharp";
+        public const string Version = "0.1.6.0";
+
+        public const string RegistryKey = "Software\\" + Company + "\\" + Product;
+        public const string RegistryValue = "XSharpPath";
+        public const string ProjectDll = "XSharp.ProjectSystem";
+
+
+    }
+}

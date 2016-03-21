dd377721c33034811d8bc2fb9059fbedf74ed2fa
Robert van der Hulst <robert@heliks.nl>
2016-03-21 15:18:22 +0100

    [doc] various doc changes & inno

--- XSharp/src/Common/Constants.cs
+++ XSharp/src/Common/Constants.cs
@@ -4,7 +4,7 @@
     {
         internal const string Company = "XSharpBV";
         internal const string Product = "XSharp";
-        internal const string Version = "0.2.1.0";
+        internal const string Version = "0.2.2.0";
         internal const string Copyright = "Copyright © XSharp BV 2015-2016";
 
         internal const string RegistryKey = "Software\\" + Company + "\\" + Product;

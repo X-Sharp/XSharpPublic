b89f8b519ed2fa5cad33453d0984c783c5de43cd
Robert van der Hulst <robert@xsharp.eu>
2016-04-29 16:59:20 +0200

    Updated source files with Apache 2 license info

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -1,4 +1,9 @@
-﻿namespace XSharp
+﻿//
+// Copyright (c) XSharp B.V.  All Rights Reserved.  
+// Licensed under the Apache License, Version 2.0.  
+// See License.txt in the project root for license information.
+//
+namespace XSharp
 {
     internal static class Constants
     {
--- VisualStudio/XSharpCodeDomProvider/XSharpTreeDiscover.cs
+++ VisualStudio/XSharpCodeDomProvider/XSharpTreeDiscover.cs
@@ -1,4 +1,9 @@
-﻿using LanguageService.CodeAnalysis.XSharp.SyntaxParser;
+﻿//
+// Copyright (c) XSharp B.V.  All Rights Reserved.  
+// Licensed under the Apache License, Version 2.0.  
+// See License.txt in the project root for license information.
+//
+using LanguageService.CodeAnalysis.XSharp.SyntaxParser;
 using LanguageService.SyntaxTree;
 using LanguageService.SyntaxTree.Misc;
 using System;

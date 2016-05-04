dcf42217f61c6cf06a920a8fc76a9132c442f333
Robert van der Hulst <robert@xsharp.eu>
2016-05-04 10:59:43 +0200

    [setup] Beta 4 installer

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -9,7 +9,7 @@ namespace XSharp
     {
         internal const string Company = "XSharpBV";
         internal const string Product = "XSharp";
-        internal const string Version = "0.2.3.0";
+        internal const string Version = "0.2.4.0";
         internal const string Copyright = "Copyright © XSharp BV 2015-2016";
 
         internal const string RegistryKey = @"Software\" + Company + @"\" + Product;

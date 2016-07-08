5aff7bb3153b48a09b1060ef1dc4b67b0c917a66
Robert van der Hulst <robert@xsharp.eu>
2016-07-08 17:55:26 +0200

    Added  CPS based project system for completeness. This is not used at this moment

---
+++ VisualStudio/ProjectPackageCPS/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using XSharp;
+[assembly: Microsoft.VisualStudio.Shell.ProvideCodeBase(CodeBase = @"$PackageFolder$\"+Constants.ProjectDll+".dll")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XSharp.ProjectType")]
+[assembly: AssemblyDescription("")]

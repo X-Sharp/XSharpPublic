1b82ec22d941c028c09d6c9c9118e9f2e417ed1b
Robert van der Hulst <robert@xsharp.eu>
2016-10-04 09:53:42 +0200

    [Compiler & Doc ] Final Beta 7a changes

--- XSharp/createhelp.cmd
+++ XSharp/createhelp.cmd
@@ -1,7 +1,16 @@
 @echo off
-SET HM="C:\Program Files (x86)\EC Software\HelpAndManual7\HELPMAN.EXE" D:\Xsharp\Dev\XSharp\docs\XSHelp.hmxp /stdout
-%HM% "/tsk=Recent Publishes:Publish as CHM" 
-%HM% "/tsk=Recent Publishes:Publish as HTM" 
-%HM% "/tsk=Recent Publishes:VS Help" 
-%HM% "/tsk=Recent Publishes:Publish as PDF"
+SET OUT=Binaries\Help\
+del %OUT%xsvulcan.cab
+del %OUT%xsharp.cab
+del %OUT%xsharp.mshc
+del %OUT%xsharp.chm
+del %OUT%xsharp.chw
+del %OUT%xsharp.pdf
+del %OUT%helpcontentsetup.msha
+SET HM="C:\Program Files (x86)\EC Software\HelpAndManual7\HELPMAN.EXE" \Xsharp\Dev\XSharp\docs\XSHelp.hmxp /stdout
+%HM% "/tsk=Recent Publishes:Publish as CHM" "/tsk=Recent Publishes:Publish as HTM"  "/tsk=Recent Publishes:VS Help"  "/tsk=Recent Publishes:Publish as PDF"
+makecab %OUT%xsvulcan.mshc %OUT%xsvulcan.cab
+makecab %OUT%xsharp.mshc %OUT%xsharp.cab
 SET HM=
+SET OUT=
+pause

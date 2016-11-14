224f44b96ce8030b33e35dd2b03602d8687e5c49
Robert van der Hulst <robert@xsharp.eu>
2016-11-14 11:05:44 +0100

    [Compiler] Fixes C288, C292 and C293

--- XSharp/src/Compiler/XSharpCodeAnalysis/Binder/Semantics/OverloadResolution.cs
+++ XSharp/src/Compiler/XSharpCodeAnalysis/Binder/Semantics/OverloadResolution.cs
@@ -20,7 +20,7 @@ namespace Microsoft.CodeAnalysis.CSharp
         {
             result = BetterResult.Neither;
             // Prefer the member not declared in VulcanRT, if applicable
-            if (Compilation.Options.IsDialectVO )
+            if (Compilation.Options.IsDialectVO)
             {
                 var asm1 = m1.Member.ContainingAssembly;
                 var asm2 = m2.Member.ContainingAssembly;
@@ -52,44 +52,198 @@ namespace Microsoft.CodeAnalysis.CSharp
             return false;
         }
 
-        //private bool VOBetterConversionFromExpression(BoundExpression node, TypeSymbol t1, TypeSymbol t2, out BetterResult result )
-        //{
-        //    // Check for SignedNess
-        //    result = BetterResult.Neither;
-        //    if (Compilation.Options.IsDialectVO && node.Type != null)
-        //    {
-        //        if (node.Type.SpecialType.IsIntegralType())
-        //        {
-        //            if (node.Type.SpecialType.IsSignedIntegralType())
-        //            {
-        //                if (t1.SpecialType.IsSignedIntegralType())
-        //                {
-        //                    result = BetterResult.Left;
-        //                    return true;
-        //                }
-        //                if (t2.SpecialType.IsSignedIntegralType())
-        //                {
-        //                    result = BetterResult.Right;
-        //                    return true;
-        //                }
-        //            }
-        //            else
-        //            {
-        //                if (!t1.SpecialType.IsSignedIntegralType())
-        //                {
-        //                     result = BetterResult.Left;
-        //                    return true;
-        //                }
-        //                if (!t2.SpecialType.IsSignedIntegralType())
-        //                {
-        //                    result = BetterResult.Right;
-        //                    return true;
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return false;
-        //}
+        private BetterResult VoBetterOperator(BinaryOperatorSignature op1, BinaryOperatorSignature op2, BoundExpression left, BoundExpression right, ref HashSet<DiagnosticInfo> useSiteDiagnostics)
+        {
+            // When the binary operators are equal we inspect the types
+            if ((op1.Kind & BinaryOperatorKind.OpMask) == (op2.Kind & BinaryOperatorKind.OpMask))
+            {
+                if ((op1.Kind & BinaryOperatorKind.TypeMask) == BinaryOperatorKind.Float && 
+                    (op2.Kind & BinaryOperatorKind.TypeMask) == BinaryOperatorKind.Double)
+                {
+                    // Lhs = real4, rhs = real8, choose real8
 
+                    return BetterResult.Right;
+                }
+                if ((op1.Kind & BinaryOperatorKind.TypeMask) == BinaryOperatorKind.Double)
+                {
+                    // rhs = numeric, lhs = double choose double
+                    switch (op2.Kind & BinaryOperatorKind.TypeMask)
+                    {
+                        case BinaryOperatorKind.Int:
+                        case BinaryOperatorKind.UInt:
+                        case BinaryOperatorKind.Long:
+                        case BinaryOperatorKind.ULong:
+                        case BinaryOperatorKind.Float:
+                        case BinaryOperatorKind.Decimal:
+                            return BetterResult.Left;
+                    }
+                }
+                if ((op2.Kind & BinaryOperatorKind.TypeMask) == BinaryOperatorKind.Double)
+                {
+                    // lhs = numeric, rhs = double choose double
+                    switch (op1.Kind & BinaryOperatorKind.TypeMask)
+                    {
+                        case BinaryOperatorKind.Int:
+                        case BinaryOperatorKind.UInt:
+                        case BinaryOperatorKind.Long:
+                        case BinaryOperatorKind.ULong:
+                        case BinaryOperatorKind.Float:
+                        case BinaryOperatorKind.Decimal:
+                            return BetterResult.Right;
+                    }
+                }
+                if (left.Type != null && right.Type != null)
+                {
+                    bool enumL = left.Type.IsEnumType() || left.Type.IsNullableType() && left.Type.GetNullableUnderlyingType().IsEnumType();
+                    bool enumR = right.Type.IsEnumType() || right.Type.IsNullableType() && right.Type.GetNullableUnderlyingType().IsEnumType();
+                    if (enumL ^ enumR)
+                    {
+                        bool enum1 = (op1.LeftType.IsEnumType() || op1.LeftType.IsNullableType() && op1.LeftType.GetNullableUnderlyingType().IsEnumType())
+                            && (op1.RightType.IsEnumType() || op1.RightType.IsNullableType() && op1.RightType.GetNullableUnderlyingType().IsEnumType());
+                        bool enum2 = (op2.LeftType.IsEnumType() || op2.LeftType.IsNullableType() && op2.LeftType.GetNullableUnderlyingType().IsEnumType())
+                            && (op2.RightType.IsEnumType() || op2.RightType.IsNullableType() && op2.RightType.GetNullableUnderlyingType().IsEnumType());
+                        if (enum1 && !enum2)
+                        {
+                            return BetterResult.Left;
+                        }
+                        else if (!enum1 && enum2)
+                            return BetterResult.Right;
+                    }
+                    // when /vo4 is enabled then we may end up having duplicate candidates
+                    // we decide here which one takes precedence
+                    if (Compilation.Options.VOSignedUnsignedConversion)
+                    {
+                        #region Integral Binary Operators
+                        if (left.Type.IsIntegralType()  && right.Type.IsIntegralType()
+                            && op1.Kind.IsIntegral() && op2.Kind.IsIntegral() )
+                        {
+                            // when both operands have integral types, choose the one that match the sign and or size 
+                            // we check the lhs of the expression first
+                            bool exprSigned = left.Type.SpecialType.IsSignedIntegralType();
+                            bool op1Signed = op1.LeftType.SpecialType.IsSignedIntegralType();
+                            bool op2Signed = op2.LeftType.SpecialType.IsSignedIntegralType();
+                            int exprSize = left.Type.SpecialType.SizeInBytes();
+                            int op1Size = op1.LeftType.SpecialType.SizeInBytes();
+                            int op2Size = op2.LeftType.SpecialType.SizeInBytes();
+                            // op1 matches sign and size and op2 does not
+                            if ((exprSigned  == op1Signed && exprSize == op1Size)
+                                && (exprSigned != op2Signed || exprSize != op2Size))
+                            {
+                                return BetterResult.Left;
+                            }
+                            // op2 matches sign and size and op1 does not
+                            if ((exprSigned != op1Signed || exprSize != op1Size)
+                                && (exprSigned == op2Signed  && exprSize == op2Size))
+                            {
+                                return BetterResult.Right;
+                            }
+                            // When we get here they both match or both do not match the sign and size
+                            // now check the rhs of the expression, to see if this helps to decide
+                            exprSigned = right.Type.SpecialType.IsSignedIntegralType();
+                            exprSize = right.Type.SpecialType.SizeInBytes();
+                            op1Signed = op1.RightType.SpecialType.IsSignedIntegralType();
+                            op2Signed = op2.RightType.SpecialType.IsSignedIntegralType();
+                            // when still undecided then choose the one where the size matches best
+                            // op1 matches sign and size and op2 does not
+                            if ((exprSigned == op1Signed && exprSize == op1Size)
+                                && (exprSigned != op2Signed || exprSize != op2Size))
+                            {
+                                return BetterResult.Left;
+                            }
+                            // op2 matches sign and size and op1 does not
+                            if ((exprSigned != op1Signed || exprSize != op1Size)
+                                && (exprSigned == op2Signed && exprSize == op2Size))
+                            {
+                                return BetterResult.Right;
+                            }
+                            // still no match. Forget the size and check only on sign
+                            exprSigned = left.Type.SpecialType.IsSignedIntegralType();
+                            op1Signed = op1.LeftType.SpecialType.IsSignedIntegralType();
+                            op2Signed = op2.LeftType.SpecialType.IsSignedIntegralType();
+                            // op1 matches sign and op2 does not
+                            if (exprSigned == op1Signed  && exprSigned != op2Signed )
+                            {
+                                return BetterResult.Left;
+                            }
+                            // op2 matches sign and op1 does not
+                            if (exprSigned != op1Signed && exprSigned == op2Signed)
+                            {
+                                return BetterResult.Right;
+                            }
+                            exprSigned = right.Type.SpecialType.IsSignedIntegralType();
+                            op1Signed = op1.RightType.SpecialType.IsSignedIntegralType();
+                            op2Signed = op2.RightType.SpecialType.IsSignedIntegralType();
+                            // op1 matches sign and op2 does not
+                            if (exprSigned == op1Signed && exprSigned != op2Signed)
+                            {
+                                return BetterResult.Left;
+                            }
+                            // op2 matches sign and op1 does not
+                            if (exprSigned != op1Signed && exprSigned == op2Signed)
+                            {
+                                return BetterResult.Right;
+                            }
+                        }
+                        #endregion
+                    }
+                }
+
+            }
+            // Solve Literal operations such as generated by ForNext statement
+            if (right.Kind == BoundKind.Literal && op1.LeftType == left.Type)
+            {
+                if (left.Type.SpecialType.IsSignedIntegralType())     // When signed, always Ok
+                    return BetterResult.Left;
+                else if (left.Type.SpecialType.IsIntegralType())      // Unsigned integral, so check for overflow
+                {
+
+                    var constValue = ((BoundLiteral)right).ConstantValue;
+                    if (constValue.IsIntegral && constValue.Int64Value >= 0)
+                    {
+                        return BetterResult.Left;
+                    }
+}
+                else // not integral, so most likely floating point
+                {
+                    return BetterResult.Left;
+                }
+            }
+            if (left.Kind == BoundKind.Literal && op1.RightType == right.Type)
+            {
+                if (right.Type.SpecialType.IsSignedIntegralType())     // When signed, always Ok
+                    return BetterResult.Left;
+                else if (right.Type.SpecialType.IsIntegralType())      // Unsigned integral, so check for overflow
+                {
+
+                    var constValue = ((BoundLiteral)left).ConstantValue;
+                    if (constValue.IsIntegral && constValue.Int64Value >= 0)
+                    {
+                        return BetterResult.Left;
+                    }
+                }
+                else // not integral, so most likely floating point
+                {
+                    return BetterResult.Left;
+                }
+            }
+            if (Compilation.Options.IsDialectVO)
+            {
+                if (!left.Type.IsUsual())
+                {
+                    if (!op1.RightType.IsUsual() && op2.RightType.IsUsual())
+                        return BetterResult.Left;
+                    if (!op2.RightType.IsUsual() && op1.RightType.IsUsual())
+                        return BetterResult.Right;
+                }
+                if (!right.Type.IsUsual())
+                {
+                    if (!op1.LeftType.IsUsual() && op2.LeftType.IsUsual())
+                        return BetterResult.Left;
+                    if (!op2.LeftType.IsUsual() && op1.LeftType.IsUsual())
+                        return BetterResult.Right;
+                }
+            }
+            return BetterResult.Neither;
+        }
     }
 }

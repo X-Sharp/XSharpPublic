1e42f9bceab16bc52ad5fd1fe1bc63f760b9fa28
Robert van der Hulst <robert@xsharp.eu>
2016-11-15 13:14:14 +0100

    [Doc] Changes

--- XSharp/src/Common/Constants.cs
+++ XSharp/src/Common/Constants.cs
@@ -20,7 +20,7 @@ namespace XSharp
         internal const string Company = "XSharp BV";
         internal const string RegCompany = "XSharpBV";
         internal const string Product = "XSharp";
-        internal const string Version = "0.2.7.2";
+        internal const string Version = "0.2.7.3";
         internal const string Copyright = "Copyright © XSharp BV 2015-2016";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;

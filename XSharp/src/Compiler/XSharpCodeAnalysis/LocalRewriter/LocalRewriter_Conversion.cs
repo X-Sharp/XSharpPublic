dbd7fc513c0cf2fdb945487f7654ee367983da58
Robert van der Hulst <robert@xsharp.eu>
2016-12-31 12:22:38 +0100

    [Compiler]  Fixes C375: for Usual -> PTR conversions we now tell Roslyn that it needs Unboxing. And inside LocalRewriter.UnBoxVOType we handle the conversion

--- XSharp/src/Compiler/XSharpCodeAnalysis/LocalRewriter/LocalRewriter_Conversion.cs
+++ XSharp/src/Compiler/XSharpCodeAnalysis/LocalRewriter/LocalRewriter_Conversion.cs
@@ -25,6 +25,28 @@ namespace Microsoft.CodeAnalysis.CSharp
 {
     internal partial class LocalRewriter
     {
+
+        // Cache the Usual2Ptr conversion since it may be used quite often and there are 43 overloads for op_Implicit.
+        private MethodSymbol _usual2Ptr;
+        private MethodSymbol getUsual2PtrOperator()
+        {
+            if (_usual2Ptr == null)
+            {
+                var usualType = _compilation.GetWellKnownType(WellKnownType.Vulcan___Usual);
+                var ptrType = _compilation.GetSpecialType(SpecialType.System_IntPtr); ;
+                var members = usualType.GetMembers("op_Implicit");
+                foreach (MethodSymbol m in members)
+                {
+                    if (m.ReturnType == ptrType)
+                    {
+                        _usual2Ptr = m;
+                        break;
+                    }
+
+                }
+            }
+            return _usual2Ptr;
+        }
         private ConversionKind UnBoxVOType(ref BoundExpression rewrittenOperand, ConversionKind conversionKind, TypeSymbol rewrittenType)
         {
             if (_compilation.Options.IsDialectVO)
@@ -35,10 +57,25 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     nts = nts.ConstructedFrom;
                 }
-                if (nts != null && nts == usualType)
+                if (nts == usualType)
                 {
-                    rewrittenOperand = _factory.StaticCall(usualType, "ToObject", rewrittenOperand);
-                    conversionKind = rewrittenType.IsObjectType() ? ConversionKind.Identity : rewrittenType.IsReferenceType ? ConversionKind.ImplicitReference : ConversionKind.Unboxing;
+                    if (rewrittenType.IsPointerType())
+                    {
+                        // Pointer types are not really boxed
+                        // we call the appropriate implicit operator here
+                        MethodSymbol m = getUsual2PtrOperator();
+                        if (m != null)
+                        { 
+                            rewrittenOperand = _factory.StaticCall(rewrittenType, m, rewrittenOperand);
+                            rewrittenOperand.WasCompilerGenerated = true;
+                            return ConversionKind.PointerToPointer; 
+                        }
+                    }
+                    else // System.Decimals, Objects and reference types
+                    {
+                        rewrittenOperand = _factory.StaticCall(usualType, "ToObject", rewrittenOperand);
+                        conversionKind = rewrittenType.IsObjectType() ? ConversionKind.Identity : rewrittenType.IsReferenceType ? ConversionKind.ImplicitReference : ConversionKind.Unboxing;
+                    }
                 }
             }
             return conversionKind;

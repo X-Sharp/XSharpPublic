5b47cb8012d1b882a8eb4251cb589b80e8444d03
Robert van der Hulst <robert@xsharp.eu>
2017-02-06 18:25:34 +0100

    Added new build events property page derived from Wix code and various support classes. Build events are config independent.

--- VisualStudio/ProjectPackage/Wix/XHelperMethods.cs
+++ VisualStudio/ProjectPackage/Wix/XHelperMethods.cs
@@ -3,11 +3,6 @@
 // Licensed under the Apache License, Version 2.0.
 // See License.txt in the project root for license information.
 //
-//
-// Copyright (c) XSharp B.V.  All Rights Reserved.
-// Licensed under the Apache License, Version 2.0.
-// See License.txt in the project root for license information.
-//
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;

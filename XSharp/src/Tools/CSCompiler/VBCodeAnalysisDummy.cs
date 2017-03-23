b7977950fe0a3edae74fe332f870f84bb14109b9
Nikos Kokkalis <nikos.v.kokkalis@gmail.com>
2017-03-23 09:34:22 +0200

    [compiler] Fix CSCompiler build

---
+++ XSharp/src/Tools/CSCompiler/VBCodeAnalysisDummy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Roslyn.Utilities;
+
+namespace Microsoft.CodeAnalysis.VisualBasic
+{
+    internal class VisualBasicCommandLineParser: CSharp.CSharpCommandLineParser
+    {
+
+    }
+    internal abstract class VisualBasicCompiler : CSharp.CSharpCompiler
+    {
+        internal VisualBasicCompiler(
+            CSharp.CSharpCommandLineParser clp,
+            string rspFile,
+            string[] args,
+            BuildPaths buildPaths,
+            string libDirectory,
+            IAnalyzerAssemblyLoader analyzerLoader)
+            : base(clp, rspFile, args, buildPaths, "", analyzerLoader)
+        {
+        }
+    }
+}

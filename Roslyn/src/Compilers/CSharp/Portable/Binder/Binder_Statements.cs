668e0269553dd889d0174f8836ed81cd77b2b320
Robert van der Hulst <robert@xsharp.eu>
2017-06-06 18:33:48 +0200

    Better fix for C527

--- Roslyn/src/Compilers/CSharp/Portable/Binder/Binder_Statements.cs
+++ Roslyn/src/Compilers/CSharp/Portable/Binder/Binder_Statements.cs
@@ -1789,23 +1789,7 @@ namespace Microsoft.CodeAnalysis.CSharp
             {
                 // Build bound conversion. The node might not be used if this is a dynamic conversion 
                 // but diagnostics should be reported anyways.
-#if XSHARP
-                var t1 = op1.Type;
-                var block = false;
-                if (op1.Kind == BoundKind.DynamicMemberAccess && 
-                    t1 == Compilation.GetWellKnownType(WellKnownType.Vulcan___Usual))
-                {
-                    t1 = Compilation.GetWellKnownType(WellKnownType.Vulcan_Codeblock);
-                    block = true;
-                }
-                var conversion = GenerateConversionForAssignment(t1, op2, diagnostics);
-                if (block)
-                    op2 = conversion;
-
-#else
                 var conversion = GenerateConversionForAssignment(op1.Type, op2, diagnostics);
-#endif
-
                 // If the result is a dynamic assignment operation (SetMember or SetIndex), 
                 // don't generate the boxing conversion to the dynamic type.
                 // Leave the values as they are, and deal with the conversions at runtime.
@@ -1815,6 +1799,16 @@ namespace Microsoft.CodeAnalysis.CSharp
                 {
                     op2 = conversion;
                 }
+#if XSHARP
+                //Dynamic operation for VO & Latebinding -> use conversion
+                else if (Compilation.Options.IsDialectVO && 
+                    Compilation.Options.LateBinding &&
+                    op1.Type == Compilation.GetWellKnownType(WellKnownType.Vulcan___Usual))
+                {
+                    op2 = conversion;
+                }
+
+#endif
             }
 
             TypeSymbol type;

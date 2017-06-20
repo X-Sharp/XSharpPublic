fa9d1c6df341d537a58b837fc71930db15f694f0
Robert van der Hulst <robert@xsharp.eu>
2017-06-20 14:36:10 +0200

    Added try catch in several entrypoints

--- VisualStudio/ProjectPackage/Editors/PeekDefinition/XSharpPeekItemSource.cs
+++ VisualStudio/ProjectPackage/Editors/PeekDefinition/XSharpPeekItemSource.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.Text;
 using System;
 using LanguageService.SyntaxTree;
 using XSharpLanguage;
+using System.Diagnostics;
 
 namespace XSharp.Project
 {
@@ -20,38 +21,46 @@ namespace XSharp.Project
 
         public void AugmentPeekSession(IPeekSession session, IList<IPeekableItem> peekableItems)
         {
-            if (!string.Equals(session.RelationshipName, PredefinedPeekRelationships.Definitions.Name, StringComparison.OrdinalIgnoreCase))
+            try
             {
-                return;
+                if (!string.Equals(session.RelationshipName, PredefinedPeekRelationships.Definitions.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return;
+                }
+                //
+                var tp = session.GetTriggerPoint(_textBuffer.CurrentSnapshot);
+                if (!tp.HasValue)
+                {
+                    return;
+                }
+                //
+                var triggerPoint = tp.Value;
+                string fileName = EditorHelpers.GetDocumentFileName(session.TextView.TextBuffer);
+                IToken stopToken;
+                //
+                List<String> tokenList = XSharpTokenTools.GetTokenList(triggerPoint.Position, triggerPoint.GetContainingLine().LineNumber, _textBuffer.CurrentSnapshot.GetText(), out stopToken, false, fileName);
+                // Check if we can get the member where we are
+                XSharpModel.XTypeMember member = XSharpLanguage.XSharpTokenTools.FindMember(triggerPoint.Position, fileName);
+                XSharpModel.XType currentNamespace = XSharpLanguage.XSharpTokenTools.FindNamespace(triggerPoint.Position, fileName);
+                // LookUp for the BaseType, reading the TokenList (From left to right)
+                CompletionElement gotoElement;
+                String currentNS = "";
+                if (currentNamespace != null)
+                {
+                    currentNS = currentNamespace.Name;
+                }
+                XSharpModel.CompletionType cType = XSharpLanguage.XSharpTokenTools.RetrieveType(fileName, tokenList, member, currentNS, stopToken, out gotoElement);
+                //
+                if ((gotoElement != null) && (gotoElement.XSharpElement != null))
+                {
+                    peekableItems.Add(new XSharpDefinitionPeekItem(gotoElement.XSharpElement, _peekResultFactory));
+                }
             }
-            //
-            var tp = session.GetTriggerPoint(_textBuffer.CurrentSnapshot);
-            if (!tp.HasValue)
+            catch (Exception ex)
             {
-                return;
+                Trace.WriteLine("XSharpPeekItemSource.AugmentPeekSession Exception : " + ex.Message);
             }
-            //
-            var triggerPoint = tp.Value;
-            string fileName = EditorHelpers.GetDocumentFileName(session.TextView.TextBuffer);
-            IToken stopToken;
-            //
-            List<String> tokenList = XSharpTokenTools.GetTokenList(triggerPoint.Position, triggerPoint.GetContainingLine().LineNumber, _textBuffer.CurrentSnapshot.GetText(), out stopToken, false, fileName);
-            // Check if we can get the member where we are
-            XSharpModel.XTypeMember member = XSharpLanguage.XSharpTokenTools.FindMember(triggerPoint.Position, fileName);
-            XSharpModel.XType currentNamespace = XSharpLanguage.XSharpTokenTools.FindNamespace(triggerPoint.Position, fileName);
-            // LookUp for the BaseType, reading the TokenList (From left to right)
-            CompletionElement gotoElement;
-            String currentNS = "";
-            if (currentNamespace != null)
-            {
-                currentNS = currentNamespace.Name;
-            }
-            XSharpModel.CompletionType cType = XSharpLanguage.XSharpTokenTools.RetrieveType(fileName, tokenList, member, currentNS, stopToken, out gotoElement);
-            //
-            if ( (gotoElement != null) && ( gotoElement.XSharpElement != null ) )
-            {
-                peekableItems.Add(new XSharpDefinitionPeekItem(gotoElement.XSharpElement, _peekResultFactory));
-            }
+
         }
 
         public void Dispose()
--- VisualStudio/ProjectPackage/Editors/XSharpQuickInfo.cs
+++ VisualStudio/ProjectPackage/Editors/XSharpQuickInfo.cs
@@ -171,7 +171,6 @@ namespace XSharp.Project
                         }
 
                     }
-
                     return;
                 }
             }
@@ -231,22 +230,29 @@ namespace XSharp.Project
 
             private void OnTextViewMouseHover(object sender, MouseHoverEventArgs e)
             {
-                //find the mouse position by mapping down to the subject buffer
-                SnapshotPoint? point = m_textView.BufferGraph.MapDownToFirstMatch
-                     (new SnapshotPoint(m_textView.TextSnapshot, e.Position),
-                    PointTrackingMode.Positive,
-                    snapshot => m_subjectBuffers.Contains(snapshot.TextBuffer),
-                    PositionAffinity.Predecessor);
-
-                if (point != null)
+                try
                 {
-                    ITrackingPoint triggerPoint = point.Value.Snapshot.CreateTrackingPoint(point.Value.Position,
-                    PointTrackingMode.Positive);
+                    //find the mouse position by mapping down to the subject buffer
+                    SnapshotPoint? point = m_textView.BufferGraph.MapDownToFirstMatch
+                         (new SnapshotPoint(m_textView.TextSnapshot, e.Position),
+                        PointTrackingMode.Positive,
+                        snapshot => m_subjectBuffers.Contains(snapshot.TextBuffer),
+                        PositionAffinity.Predecessor);
 
-                    if (!m_provider.QuickInfoBroker.IsQuickInfoActive(m_textView))
+                    if (point != null)
                     {
-                        m_session = m_provider.QuickInfoBroker.TriggerQuickInfo(m_textView, triggerPoint, true);
+                        ITrackingPoint triggerPoint = point.Value.Snapshot.CreateTrackingPoint(point.Value.Position,
+                        PointTrackingMode.Positive);
+
+                        if (!m_provider.QuickInfoBroker.IsQuickInfoActive(m_textView))
+                        {
+                            m_session = m_provider.QuickInfoBroker.TriggerQuickInfo(m_textView, triggerPoint, true);
+                        }
                     }
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine("XSharpQuickInfo.OnTextViewMouseHover Exception : " + ex.Message);
                 }
             }
 

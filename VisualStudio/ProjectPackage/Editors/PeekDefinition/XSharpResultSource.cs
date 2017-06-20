34faad2895d629c5d59607786812011e539ec1b1
Robert van der Hulst <robert@xsharp.eu>
2017-06-20 14:36:10 +0200

    Added try catch in several entrypoints

--- VisualStudio/ProjectPackage/Editors/PeekDefinition/XSharpResultSource.cs
+++ VisualStudio/ProjectPackage/Editors/PeekDefinition/XSharpResultSource.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Imaging.Interop;
+using System.Diagnostics;
 
 namespace XSharp.Project
 {
@@ -20,35 +21,42 @@ namespace XSharp.Project
 
         public void FindResults(string relationshipName, IPeekResultCollection resultCollection, CancellationToken cancellationToken, IFindPeekResultsCallback callback)
         {
-            if (relationshipName != PredefinedPeekRelationships.Definitions.Name)
+            try
             {
-                return;
+                if (relationshipName != PredefinedPeekRelationships.Definitions.Name)
+                {
+                    return;
+                }
+                var fileName = Path.GetFileName(this.peekableItem._gotoElement.File.FullPath);
+                var label = this.peekableItem._gotoElement.Name;
+                var title = string.Format("{0} - ({1}, {2})", fileName, this.peekableItem._gotoElement.Range.StartLine, this.peekableItem._gotoElement.Range.StartColumn);
+
+                using (var displayInfo = new PeekResultDisplayInfo2(label: label, labelTooltip: this.peekableItem._gotoElement.File.FullPath, title: title, titleTooltip: this.peekableItem._gotoElement.File.FullPath, startIndexOfTokenInLabel: 0, lengthOfTokenInLabel: label.Length))
+                {
+                    var result = peekableItem._peekResultFactory.Create
+                    (
+                        displayInfo,
+                        default(ImageMoniker),
+                        this.peekableItem._gotoElement.File.FullPath,
+                        this.peekableItem._gotoElement.Range.StartLine - 1,
+                        this.peekableItem._gotoElement.Range.StartColumn - 1,
+                        this.peekableItem._gotoElement.Range.EndLine - 1,
+                        this.peekableItem._gotoElement.Range.EndColumn - 1,
+                        this.peekableItem._gotoElement.Range.StartLine - 1,
+                        this.peekableItem._gotoElement.Range.StartColumn - 1,
+                        this.peekableItem._gotoElement.Range.EndLine - 1,
+                        this.peekableItem._gotoElement.Range.EndColumn - 1,
+                        false,
+                        new Guid(XSharpConstants.EditorFactoryGuidString)
+                    );
+
+                    resultCollection.Add(result);
+                    callback.ReportProgress(1);
+                }
             }
-            var fileName = Path.GetFileName(this.peekableItem._gotoElement.File.FullPath);
-            var label = this.peekableItem._gotoElement.Name;
-            var title = string.Format("{0} - ({1}, {2})", fileName, this.peekableItem._gotoElement.Range.StartLine, this.peekableItem._gotoElement.Range.StartColumn);
-
-            using (var displayInfo = new PeekResultDisplayInfo2(label: label, labelTooltip: this.peekableItem._gotoElement.File.FullPath, title: title, titleTooltip: this.peekableItem._gotoElement.File.FullPath, startIndexOfTokenInLabel: 0, lengthOfTokenInLabel: label.Length))
+            catch (Exception ex)
             {
-                var result = peekableItem._peekResultFactory.Create
-                (
-                    displayInfo,
-                    default(ImageMoniker),
-                    this.peekableItem._gotoElement.File.FullPath,
-                    this.peekableItem._gotoElement.Range.StartLine - 1,
-                    this.peekableItem._gotoElement.Range.StartColumn - 1,
-                    this.peekableItem._gotoElement.Range.EndLine - 1,
-                    this.peekableItem._gotoElement.Range.EndColumn - 1,
-                    this.peekableItem._gotoElement.Range.StartLine - 1,
-                    this.peekableItem._gotoElement.Range.StartColumn - 1,
-                    this.peekableItem._gotoElement.Range.EndLine - 1,
-                    this.peekableItem._gotoElement.Range.EndColumn - 1,
-                    false,
-                    new Guid(XSharpConstants.EditorFactoryGuidString)
-                );
-
-                resultCollection.Add(result);
-                callback.ReportProgress(1);
+                Trace.WriteLine("XSharpResultSource.FindResults Exception : " + ex.Message);
             }
         }
 

1c7ae61cae24924fa1ec1baba8d83789e23c905c
Robert van der Hulst <robert@xsharp.eu>
2017-06-27 21:08:02 +0200

    Fix 2 problems reported by Chris

--- VisualStudio/XSharpColorizer/Extensions.cs
+++ VisualStudio/XSharpColorizer/Extensions.cs
@@ -17,11 +17,14 @@ namespace XSharpColorizer
         {
             int start = span.Start;
             int length = span.Length;
-            // validate
-            if (span.End > snapshot.Length)
+            // validate parameters
+            start = start < 0 ? 0 : start;
+            start = start > snapshot.Length ? snapshot.Length : start;
+            if (start + length > snapshot.Length)
             {
                 length = snapshot.Length - start;
             }
+            length = length < 0 ? 0 : length;
             var sspan = new SnapshotSpan(snapshot, start, length);
             var tag = new ClassificationTag(classificationType);
             return new TagSpan<IClassificationTag>(sspan, tag);
@@ -31,11 +34,15 @@ namespace XSharpColorizer
         {
             int start = span.Start;
             int length = span.Length;
-            // validate
-            if (span.End > snapshot.Length)
+            // validate parameters
+            // validate parameters
+            start = start < 0 ? 0 : start;
+            start = start > snapshot.Length ? snapshot.Length : start;
+            if (start + length > snapshot.Length)
             {
                 length = snapshot.Length - start;
             }
+            length = length < 0 ? 0 : length;
             SnapshotSpan sspan = new SnapshotSpan(snapshot, start, length);
             return new ClassificationSpan(sspan, classificationType);
         }

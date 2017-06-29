e0eef9e570d40ecc25198f103c2051aff36a9298
Robert van der Hulst <robert@xsharp.eu>
2017-06-29 12:13:11 +0200

    Changed default for missing  "NostandardDefs" back to FALSE

--- VisualStudio/ProjectPackage/PropertyPages/XSharpBuildPropertyPage.cs
+++ VisualStudio/ProjectPackage/PropertyPages/XSharpBuildPropertyPage.cs
@@ -361,7 +361,7 @@ namespace XSharp.Project
 
             commandlineoption= getCfgString(nameof(CommandLineOption),  "");
             ppo = getCfgLogic(nameof(PPO),  false);
-            nostandarddefs = getCfgLogic(nameof(NoStandardDefs),  true);
+            nostandarddefs = getCfgLogic(nameof(NoStandardDefs),  false);
             includepaths = getCfgString(nameof(IncludePaths),  "");
             defines = getCfgString(nameof(DefineConstants), "");
 

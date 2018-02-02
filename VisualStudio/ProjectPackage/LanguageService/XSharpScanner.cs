5b4ec5195e6fdb01e6329bed5b7e218166d822b8
Robert van der Hulst <robert@xsharp.eu>
2018-02-02 11:53:24 +0100

    Class is not used at all

--- VisualStudio/ProjectPackage/LanguageService/XSharpScanner.cs
+++ VisualStudio/ProjectPackage/LanguageService/XSharpScanner.cs
@@ -1,44 +1,44 @@
-﻿//
-// Copyright (c) XSharp B.V.  All Rights Reserved.  
-// Licensed under the Apache License, Version 2.0.  
-// See License.txt in the project root for license information.
-//
-using Microsoft.VisualStudio.Package;
-using Microsoft.VisualStudio.TextManager.Interop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿////
+//// Copyright (c) XSharp B.V.  All Rights Reserved.  
+//// Licensed under the Apache License, Version 2.0.  
+//// See License.txt in the project root for license information.
+////
+//using Microsoft.VisualStudio.Package;
+//using Microsoft.VisualStudio.TextManager.Interop;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace XSharp.LanguageService
-{
-    internal class XSharpScanner : IScanner
-    {
-        private IVsTextBuffer m_buffer;
-        string m_source;
+//namespace XSharp.LanguageService
+//{
+//    internal class XSharpScanner : IScanner
+//    {
+//        private IVsTextBuffer m_buffer;
+//        string m_source;
         
 
-        public XSharpScanner(IVsTextBuffer buffer)
-        {
-            m_buffer = buffer;
-        }
+//        public XSharpScanner(IVsTextBuffer buffer)
+//        {
+//            m_buffer = buffer;
+//        }
 
-        bool IScanner.ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
-        {
-            // The caller will SetSource() to set the line that is to be parsed. 
-            // Then the ScanTokenAndProvideInfoAboutIt method is typically called repeatedly until all tokens are obtained. 
-            // The state value is cached line-by-line by the buffer wich keep a copy of it
-            // That's used to keep the current line-state
-            tokenInfo.Type = TokenType.Unknown;
-            tokenInfo.Color = TokenColor.Text;
-            // True -> Again; False -> Stop
-            return false;
-        }
+//        bool IScanner.ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
+//        {
+//            // The caller will SetSource() to set the line that is to be parsed. 
+//            // Then the ScanTokenAndProvideInfoAboutIt method is typically called repeatedly until all tokens are obtained. 
+//            // The state value is cached line-by-line by the buffer wich keep a copy of it
+//            // That's used to keep the current line-state
+//            tokenInfo.Type = TokenType.Unknown;
+//            tokenInfo.Color = TokenColor.Text;
+//            // True -> Again; False -> Stop
+//            return false;
+//        }
 
-        void IScanner.SetSource(string source, int offset)
-        {
-            m_source = source.Substring(offset);
-        }
-    }
-}
+//        void IScanner.SetSource(string source, int offset)
+//        {
+//            m_source = source.Substring(offset);
+//        }
+//    }
+//}

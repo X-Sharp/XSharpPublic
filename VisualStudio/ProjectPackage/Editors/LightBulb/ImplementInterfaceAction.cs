2b1e29e8d162d8bfd8c4a3938097b26e138ebc90
Fabrice Foray <fabrice.foray@hotmail.fr>
2018-03-15 18:12:25 +0100

    LightBulbs: Add empty shells to prepare some SuggestedActions

---
+++ VisualStudio/ProjectPackage/Editors/LightBulb/ImplementInterfaceAction.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Imaging.Interop;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text;
+using System.Threading;
+
+namespace XSharp.Project.Editors.LightBulb
+{
+    internal class ImplementInterfaceSuggestedAction : ISuggestedAction
+    {
+        private ITrackingSpan m_span;
+        private string m_upper;
+        private string m_display;
+        private ITextSnapshot m_snapshot;
+
+        public ImplementInterfaceSuggestedAction(ITrackingSpan span)
+        {
+            m_span = span;
+            m_snapshot = span.TextBuffer.CurrentSnapshot;
+            m_upper = span.GetText(m_snapshot).ToUpper();
+            m_display = string.Format("Convert '{0}' to upper case", span.GetText(m_snapshot));
+        }
+
+        public Task<object> GetPreviewAsync(CancellationToken cancellationToken)
+        {
+            var textBlock = new TextBlock();
+            textBlock.Padding = new Thickness(5);
+            textBlock.Inlines.Add(new Run() { Text = m_upper });
+            return Task.FromResult<object>(textBlock);
+        }
+
+        public Task<IEnumerable<SuggestedActionSet>> GetActionSetsAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult<IEnumerable<SuggestedActionSet>>(null);
+        }
+
+        public bool HasActionSets
+        {
+            get { return false; }
+        }
+        public string DisplayText
+        {
+            get { return m_display; }
+        }
+        public ImageMoniker IconMoniker
+        {
+            get { return default(ImageMoniker); }
+        }
+        public string IconAutomationText
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public string InputGestureText
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public bool HasPreview
+        {
+            get { return true; }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool TryGetTelemetryId(out Guid telemetryId)
+        {
+            // This is a sample action and doesn't participate in LightBulb telemetry  
+            telemetryId = Guid.Empty;
+            return false;
+        }
+
+        // The job is done here !!
+        public void Invoke(CancellationToken cancellationToken)
+        {
+            m_span.TextBuffer.Replace(m_span.GetSpan(m_snapshot), m_upper);
+        }
+
+
+    }
+}

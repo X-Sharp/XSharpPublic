851dcc48d4cb78aeb45605226b82904b04cea5dc
Fabrice Foray <fabrice.foray@hotmail.fr>
2018-03-29 22:32:19 +0200

    CommentSelection: Change the default behaviour in Source.

--- VisualStudio/ProjectPackage/LanguageService/XSharpSource.cs
+++ VisualStudio/ProjectPackage/LanguageService/XSharpSource.cs
@@ -1,36 +1,65 @@
-﻿////
-//// Copyright (c) XSharp B.V.  All Rights Reserved.  
-//// Licensed under the Apache License, Version 2.0.  
-//// See License.txt in the project root for license information.
-////
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Microsoft.VisualStudio;
-//using Microsoft.VisualStudio.Package;
-//using Microsoft.VisualStudio.TextManager.Interop;
-//using Microsoft.VisualStudio.OLE.Interop;
+﻿//
+// Copyright (c) XSharp B.V.  All Rights Reserved.  
+// Licensed under the Apache License, Version 2.0.  
+// See License.txt in the project root for license information.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
 
-//namespace XSharp.LanguageService
-//{
-//    class XSharpSource: Source
-//    {
-//        public XSharpSource( XSharpLanguageService service,
-//                        IVsTextLines textLines,
-//                        Colorizer colorizer)
-//            : base(service, textLines, colorizer)
-//        {
+namespace XSharp.LanguageService
+{
+    class XSharpSource : Source
+    {
+        public XSharpSource(XSharpLanguageService service,
+                        IVsTextLines textLines,
+                        Colorizer colorizer)
+            : base(service, textLines, colorizer)
+        {
 
-//        }
-//        public override void BeginParse()
-//        {
-//            base.BeginParse();
-//        }
-//        public override ParseRequest BeginParse(int line, int idx, TokenInfo info, ParseReason reason, IVsTextView view, ParseResultHandler callback)
-//        {
-//            return base.BeginParse(line, idx, info, reason, view, callback);
-//        }
-//    }
-//}
+        }
+
+
+        public override TextSpan CommentSpan(TextSpan span)
+        {
+            TextSpan span2 = span;
+            CommentInfo commentFormat = this.GetCommentFormat();
+            using (new CompoundAction(this, "CommentSelection"))
+            {
+                if (commentFormat.UseLineComments && !string.IsNullOrEmpty(commentFormat.LineStart))
+                {
+                    if (span.iStartIndex == 0 && span.iEndIndex == 0)
+                    {
+                        if (span.iEndLine > span.iStartLine )
+                        {
+                            span.iEndLine--;
+                        }
+                    }
+                    span2 = this.CommentLines(span, commentFormat.LineStart);
+                    return span2;
+                }
+                if (!string.IsNullOrEmpty(commentFormat.BlockStart) && !string.IsNullOrEmpty(commentFormat.BlockEnd))
+                {
+                    span2 = this.CommentBlock(span, commentFormat.BlockStart, commentFormat.BlockEnd);
+                }
+            }
+            return span2;
+        }
+
+
+        //public override void BeginParse()
+        //{
+        //    base.BeginParse();
+        //}
+        //public override ParseRequest BeginParse(int line, int idx, TokenInfo info, ParseReason reason, IVsTextView view, ParseResultHandler callback)
+        //{
+        //    return base.BeginParse(line, idx, info, reason, view, callback);
+        //}
+    }
+}

2a4e6aa6eb6abf121694105b776c22c27422299c
Robert van der Hulst <robert@xsharp.eu>
2018-05-24 15:05:40 +0200

    Updated project files and cmd files

--- XSharp/rebuild.cmd
+++ XSharp/rebuild.cmd
@@ -14,8 +14,8 @@ if exist build-debug.log del build-debug.log
 if exist build-release.log del build-release.log
 rename msbuild1.log build-debug.log
 rename msbuild2.log build-release.log
-msbuild Tools.sln /fl1 /p:Configuration=Debug	/t:ReBuild /property:OfficialBuild=true /m /v:m /nologo
-msbuild Tools.sln /fl2 /p:Configuration=Release	/t:ReBuild /property:OfficialBuild=true /m /v:m /nologo
+msbuild Tools.sln /fl1 /p:Configuration=Debug   /t:ReBuild /m /v:m /nologo
+msbuild Tools.sln /fl2 /p:Configuration=Release /t:ReBuild /m /v:m /nologo
 if exist tools-debug.log del tools-debug.log
 if exist tools-release.log del tools-release.log
 rename msbuild1.log tools-debug.log

ee353c28d7488aabd0d28802682335a69c56b050
Robert van der Hulst <robert@xsharp.eu>
2018-06-11 16:59:00 +0200

    Some build changes and public key changes

--- clean.cmd
+++ clean.cmd
@@ -1,7 +1,9 @@
 @echo off
-Echo Cleaning VsIntegration Solution
+Echo Cleaning VsIntegration and Tools
 msbuild VsIntegration.sln /fl1 /p:Configuration=Debug	/t:Clean /m /v:m 
 msbuild VsIntegration.sln /fl2 /p:Configuration=Release	/t:Clean /m /v:m 
+msbuild Tools.sln /fl1 /p:Configuration=Debug	/t:Clean /m /v:m 
+msbuild Tools.sln /fl2 /p:Configuration=Release	/t:Clean /m /v:m 
 if exist clean-debug.log del clean-debug.log
 if exist clean-release.log del clean-release.log
 rename msbuild1.log clean-debug.log

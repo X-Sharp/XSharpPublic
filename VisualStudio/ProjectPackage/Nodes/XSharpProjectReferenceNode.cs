8b0fbed22552b13689558aa18c1da502bab259d8
Robert van der Hulst <robert@xsharp.eu>
2018-06-20 18:29:03 +0200

    - updated images for several tree elements - added separate class for properties Folder - open on Properties folder opens project Properties dialog

--- VisualStudio/ProjectPackage/Nodes/XSharpProjectReferenceNode.cs
+++ VisualStudio/ProjectPackage/Nodes/XSharpProjectReferenceNode.cs
@@ -100,6 +100,13 @@ namespace XSharp.Project
             ErrorHandler.ThrowOnFailure(hierarchy.GetProperty(VSConstants.VSITEMID_ROOT, (int)__VSHPROPID.VSHPROPID_TypeName, out projectType));
             return projectType as string;
         }
+        public override int ImageIndex
+        {
+            get
+            {
+                return XSharpImageListIndex.ReferenceContainer + XSharpProjectNode.imageOffset;
+            }
+        }
 
         /// <summary>
         /// Shows Visual Studio message box with error message regarding project to project reference. Target Project must be built before

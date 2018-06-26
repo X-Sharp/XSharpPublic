fe081fe3462df7a430eeb11f4841fd8da659a42d
Robert van der Hulst <robert@xsharp.eu>
2018-06-26 14:02:54 +0200

    Build tools

--- buildrt.cmd
+++ buildrt.cmd
@@ -1,18 +1,15 @@
 @echo off
-if "%1" == "Debug" goto Ok
-if "%1" == "debug" goto Ok
-if "%1" == "Release" goto Ok
-if "%1" == "release" goto Ok
-if "%1" == "Documentation" goto Ok
-if "%1" == "documentation" goto Ok
+if /i "%1" == "Debug" goto Ok
+if /i "%1" == "Release" goto Ok
+if /i "%1" == "Documentation" goto Ok
 goto Error
 :Ok
-Echo Building Runtime 
-msbuild Runtime.sln 		/fl1 /flp1:Append /p:Configuration=%1	/p:Platform="Any CPU"     /t:Build  /m /v:m /nologo 
-if exist build-%1.log del build-%1.log
-rename msbuild1.log build-%1.log
+Echo Building Runtime %1 Configuration
+msbuild Runtime.sln 		/fl1 /flp1:Append /p:Configuration=%1	/p:Platform="Any CPU"     /t:Build  /m /v:q /nologo 
+if exist buildRt%1.log del buildRt%1.log
+rename msbuild1.log buildRt-%1.log
 Goto End
 :Error
-echo Syntax: Build Debug, Build Release or Build Documentation
+echo Syntax: BuildRt Debug, BuildRt Release or BuildRt Documentation
 :End
 
--- cleanrt.cmd
+++ cleanrt.cmd
@@ -1,14 +1,12 @@
 @echo off
 Echo Cleaning Runtime Solution
-msbuild Runtime.sln /fl1 /p:Configuration=Debug	/t:Clean /m /v:m 
-msbuild Runtime.sln /fl2 /p:Configuration=Release	/t:Clean /m /v:m 
-msbuild Runtime.sln /fl3 /p:Configuration=Documentation	/t:Clean /m /v:m 
-if exist clean-debug.log del clean-debug.log
-if exist clean-release.log del clean-release.log
-if exist clean-documentation.log del clean-documentation.log
-rename msbuild1.log clean-debug.log
-rename msbuild2.log clean-release.log
-rename msbuild3.log clean-documentation.log
+msbuild Runtime.sln /fl1 /p:Configuration=Debug			/t:Clean /m /v:q  
+msbuild Runtime.sln /fl2 /p:Configuration=Release		/t:Clean /m /v:q  
+msbuild Runtime.sln /fl3 /p:Configuration=Documentation	/t:Clean /m /v:q 
+if exist cleanrt*.log del cleanrt*.log
+rename msbuild1.log cleanrt-debug.log
+rename msbuild2.log cleanrt-release.log
+rename msbuild3.log cleanrt-documentation.log
 if exist binaries\debug\*.* rd binaries\debug /s /q
 if exist binaries\obj\*.* rd binaries\obj /s /q
 if exist binaries\release\*.* rd binaries\release /s /q

9e61f57c7bc2d1e8f2475d8849c71367a7664f8f
Robert van der Hulst <robert@xsharp.eu>
2018-07-11 22:42:44 +0200

    Beta 3 version

--- Common/BuildNumber.h
+++ Common/BuildNumber.h
@@ -1,6 +1,6 @@
 // BuildNumber.h
 #ifndef BUILDNUMBER_H
-#define PRODUCT_NAME     "XSharp Bandol Beta 2"
+#define PRODUCT_NAME     "XSharp Bandol Beta 3"
 #define PRODUCT			 "XSharp"
 #define COPYRIGHT_STR    "Copyright (c) XSharp BV 2015-2018."
 #define COMPANY_NAME     "XSharp BV"
@@ -9,8 +9,8 @@
 // This is used for the assembly version number, which the CLR uses to determine binding
 // This generally changes when we release a new full build 
 // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE
-#define VERSION_NUMBER_STR     "2.0.0.2"
-#define VERSION_NUMBER			2,0,0,2
+#define VERSION_NUMBER_STR     "2.0.0.3"
+#define VERSION_NUMBER			2,0,0,3
 
 // This is the file version number, which is ignored by .NET but used by Windows installer to determine
 // whether one file is newer than another.

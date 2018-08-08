17c07fe35e82419ef3247f25097784cef410dd9c
Robert van der Hulst <robert@xsharp.eu>
2018-08-08 18:33:06 +0200

    version 2.0.0.4

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -1,6 +1,6 @@
 ﻿//
-// Copyright (c) XSharp B.V.  All Rights Reserved.  
-// Licensed under the Apache License, Version 2.0.  
+// Copyright (c) XSharp B.V.  All Rights Reserved.
+// Licensed under the Apache License, Version 2.0.
 // See License.txt in the project root for license information.
 //
 namespace XSharp
@@ -9,11 +9,11 @@ namespace XSharp
     {
 
         internal const string Company = "XSharpBV";
-        internal const string ProductName = "XSharp Bandol Beta 3";
+        internal const string ProductName = "XSharp Bandol Beta 4";
         internal const string Product = "XSharp";
         // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE BUILDNUMBER.H FILE
-        internal const string Version = "2.0.0.3";
-        internal const string Copyright = "Copyright © XSharp BV 2015-2018"; 
+        internal const string Version = "2.0.0.4";
+        internal const string Copyright = "Copyright © XSharp BV 2015-2018";
 
         internal const string RegistryKey = @"Software\" + Company + @"\" + Product;
         internal const string RegistryKey64 = @"Software\WOW6432Node\" + Company + @"\" + Product;

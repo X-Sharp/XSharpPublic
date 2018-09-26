c49d385a07ba7f712e7fe4f18f6eb10f8d45e816
Robert van der Hulst <robert@xsharp.eu>
2018-09-26 09:01:05 +0200

    Fixes C627 (Richard Wooters)

--- XSharp/src/Compiler/XSharpCodeAnalysis/ErrorCode.cs
+++ XSharp/src/Compiler/XSharpCodeAnalysis/ErrorCode.cs
@@ -44,7 +44,6 @@ namespace Microsoft.CodeAnalysis.CSharp
     XS_WRN:ERR_CantCallSpecialMethod = 571,
     XS_WRN:ERR_ProtectedInStruct = 666,
     XS_WRN:ERR_ExplicitParamArray = 674,
-    XS_WRN:ERR_StaticDerivedFromNonObject = 713,
     XS_WRN:ERR_ProtectedInStatic = 1057,
     XS_WRN:ERR_UseDefViolationProperty = 8079,
     XS_WRN:ERR_ConversionWithBase = 553,

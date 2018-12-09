7273165ee6d9c6703eafef70dc07fabdc6ee22cf
Robert van der Hulst <robert@xsharp.eu>
2018-12-09 16:09:59 +0100

    - Moved MacroCompiler class to the MacroCompiler assembly - Created dependency in MacroCompiler assembly on XSharp.Core - restored InternalsVisibleTo to macrocompiler for macroCompilerTest

---
+++ XSharp/src/Scripting/MacroCompiler/Compiler/MacroCompiler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using XSharp.MacroCompiler;
+
+namespace XSharp.Runtime
+{
+    public class MacroCompiler : IMacroCompiler
+    {
+        private  MacroOptions options;
+        internal Compilation<object, RuntimeCodeblockDelegate> compiler ;
+
+
+        public MacroCompiler()
+        {
+            options = new MacroOptions() { AllowSingleQuotedStrings = true };
+            GetCompiler(true);
+        }
+
+        public MacroOptions Options => options;
+
+
+        private Compilation<object, RuntimeCodeblockDelegate>  GetCompiler(bool lVo)
+        {
+            if (Options.AllowOldStyleComments != lVo || compiler == null)
+            {
+                options.AllowSingleQuotedStrings = lVo;
+                compiler = Compilation.Create<object, RuntimeCodeblockDelegate>(options);
+            }
+            return compiler;
+        }
+
+        public ICodeBlock Compile(string macro, bool lAllowSingleQuotes, Module module, out bool isCodeblock)
+        {
+            isCodeblock = macro.Replace(" ", "").StartsWith("{|");
+            Compilation<object, RuntimeCodeblockDelegate> compiler = GetCompiler(lAllowSingleQuotes);
+            var m = compiler.Compile(macro);
+            if (m.Diagnostic != null)
+            {
+                throw m.Diagnostic;
+            }
+            return new RuntimeCodeblock(m.Macro, m.ParamCount);
+        }
+        public ICodeBlock Compile(string macro)
+        {
+            Compilation<object, RuntimeCodeblockDelegate> compiler = GetCompiler(true);
+            var m = compiler.Compile(macro);
+            if (m.Diagnostic != null)
+            {
+                throw m.Diagnostic;
+            }
+            return new RuntimeCodeblock(m.Macro, m.ParamCount);
+        }
+    }
+}
--- XSharp/src/Scripting/MacroCompiler/Properties/AssemblyInfo.cs
+++ XSharp/src/Scripting/MacroCompiler/Properties/AssemblyInfo.cs
@@ -22,3 +22,4 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("XSharp.Macrocompiler")]
 [assembly: AssemblyDescription("X# fast macro compiler")]
+[assembly: InternalsVisibleTo("MacroCompilerTest,PublicKey=0024000004800000940000000602000000240000525341310004000001000100b16a35b62bb33ce476c595e75bcc83fe4566c0a7cb9c093ce23e7add61fe1fc8a6edca2e542f0dc9ce41ec6b4260a73dda598c81f61a6f9522653ebfeae098a3bdb641020e843cbab825afe1c3910d42d17a1dcf211abb1cba4fc5e19569307c67a11c92b848d2df23f454d5ed1ab8b479afa4ece799445292b11012225aee96")]

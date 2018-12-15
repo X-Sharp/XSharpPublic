c688797af8e472864a63ff3139e9e268e84ebd81
Nikos Kokkalis <nikos.v.kokkalis@gmail.com>
2018-12-15 15:39:40 +0200

    [macro] Add missing file

---
+++ XSharp/src/Scripting/MacroCompiler/CodeGen/Operator.Unary.Emit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace XSharp.MacroCompiler
+{
+    using static CodeGen;
+    using Syntax;
+
+    internal partial class UnaryOperatorSymbol : TypedSymbol
+    {
+        internal override void EmitGet(ILGenerator ilg)
+        {
+            EmitUnaryOperator(ilg, this, Type); // TODO nkok: handle checked/unchecked
+        }
+    }
+
+    internal partial class UnaryOperatorSymbolWithMethod : UnaryOperatorSymbol
+    {
+        internal override void EmitGet(ILGenerator ilg)
+        {
+            ilg.Emit(OpCodes.Call, Method.Method);
+        }
+    }
+}

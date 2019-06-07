fc0ee431065958f3df5505c49c53583e51ba3052
Robert van der Hulst <robert@xsharp.eu>
2019-06-07 11:59:38 +0200

    [Common] Updated version info. Now we have separate VersionNumber, InformationalVersion/ProductVersionNumber and FileVersionNumber. InformationalVersion/ProductVersion includes "RC" description

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -13,7 +13,9 @@ namespace XSharp
         internal const string ProductName = "XSharp Bandol RC 1a";
         internal const string Product = "XSharp";
         // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE BUILDNUMBER.H FILE
-        internal const string Version = "2.0.1.1";
+        internal const string Version = "2.0.2.0";
+        internal const string FileVersion = "2.0.2.0";
+        internal const string ProductVersion = "2.0 RC2";
         internal const string Copyright = "Copyright © XSharp BV 2015-2019";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;

e4f6b7b7f2eb6de067d92f7721bc016efd38aaaf
Robert van der Hulst <robert@xsharp.eu>
2019-06-14 18:46:29 +0200

    [Doc] change

--- XSharp/src/Setup/makecabs.cmd
+++ XSharp/src/Setup/makecabs.cmd
@@ -1,13 +1,17 @@
-copy xsharp.msha \xsharp\dev\xsharp\binaries\help
-cd \xsharp\dev\xsharp\binaries\help
+set mainhelp=\xsharp\dev\xsharp\binaries\help
+set rthelp1=\xsharp\devrt\binaries\help
+set rthelp2=\xsharp\devrt\binaries\vohelp
+set setup=c:\XSharp\Dev\XSharp\src\Setup
+copy xsharp.msha %mainhelp%
+cd %mainhelp%
 makecab xsharp.mshc xsharp.cab /V1
-cd \xsharp\devrt\binaries\help
+cd %rthelp1%
 makecab xsruntime.mshc xsruntime.cab /V1
-copy xsruntime.mshc \xsharp\dev\xsharp\binaries\help
-copy xsruntime.cab \xsharp\dev\xsharp\binaries\help
-cd \xsharp\devrt\binaries\Vohelp
+copy xsruntime.mshc %mainhelp%
+copy xsruntime.cab %mainhelp%
+cd %rthelp2%
 makecab XsVoSdk.mshc XsVoSdk.cab /V1
-copy XsVoSdk.mshc \xsharp\dev\xsharp\binaries\help
-copy XsVoSdk.cab  \xsharp\dev\xsharp\binaries\help
-cd c:\XSharp\Dev\XSharp\src\Setup
+copy XsVoSdk.mshc %mainhelp%
+copy XsVoSdk.cab  %mainhelp%
+cd %setup%
 

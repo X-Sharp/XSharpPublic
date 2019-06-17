1744c8452f1eb93fea3df320db0d06f8bc9f6e44
Robert van der Hulst <robert@xsharp.eu>
2019-06-17 10:48:36 +0200

    [Compiler] Updated File Version for Interim fix

--- XSharp/src/Common/BuildNumber.h
+++ XSharp/src/Common/BuildNumber.h
@@ -12,8 +12,8 @@
     // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE
     #define VERSION_NUMBER_STR     "2.0.2.0"
     #define VERSION_NUMBER			2,0,2,0
-    #define FILEVERSION_NUMBER       2,0,2,0
-    #define FILEVERSION_NUMBER_STR   "2.0.2.0"
+    #define FILEVERSION_NUMBER       2,0,2,1
+    #define FILEVERSION_NUMBER_STR   "2.0.2.1"
     #define INFORMATIONAL_NUMBER_STR  "2.0 RC 2"
 
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine

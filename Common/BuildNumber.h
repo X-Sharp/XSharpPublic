b7677d90feb5efd2a9afdc7915230bdbe45001c2
Robert van der Hulst <robert@xsharp.eu>
2019-08-07 14:49:11 +0200

    [Runtime] Updated versions

--- Common/BuildNumber.h
+++ Common/BuildNumber.h
@@ -1,18 +1,18 @@
 // BuildNumber.h
 #ifndef BUILDNUMBER_H
     #define BUILDNUMBER_H
-    #define PRODUCT_NAME     "XSharp Bandol RC 3"
+    #define PRODUCT_NAME     "XSharp Bandol"
     #define PRODUCT			 "XSharp"
     #define COPYRIGHT_STR    "Copyright (c) XSharp BV 2015-2019."
     #define COMPANY_NAME     "XSharp BV"
     #define REG_COMPANY_NAME  "XSharpBV"
 
     // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE
-    #define VERSION_NUMBER_STR     "2.0.3.0"
-    #define VERSION_NUMBER			2,0,3,0
-    #define FILEVERSION_NUMBER       2,0,3,0
-    #define FILEVERSION_NUMBER_STR   "2.0.3.0"
-    #define INFORMATIONAL_NUMBER_STR  "2.0 RC 3"
+#define VERSION_NUMBER_STR     "2.0.0.0"
+#define VERSION_NUMBER			2,0.0,0
+#define FILEVERSION_NUMBER       2,0,4,0
+#define FILEVERSION_NUMBER_STR   "2.0.4.0"
+#define INFORMATIONAL_NUMBER_STR  "2.0 GA"
 
     #ifdef __DEBUG__
         #define ASSEMBLY_CONFIGURATION "Debug"

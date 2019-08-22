d37fe244ee997eac1d36d84bf2cbb74060a641ac
Robert van der Hulst <robert@xsharp.eu>
2019-08-22 09:06:11 +0200

    Updated Build scripts

--- XSharp/build.cmd
+++ XSharp/build.cmd
@@ -10,7 +10,7 @@ set tmpXSharpDev=%XSharpDev%
 rem Reset the XSharpDev path so we will not use the compiler we are generating
 set XSharpDev=
 taskkill  /f /t /fi "IMAGENAME eq XSCompiler.exe" >nul
-Echo Building Compiler %1 Configuration  with Master.sln
+Echo Building Compiler %1 Configuration  with Compiler.sln
 Echo Using MsBuild in %msbuilddir%
 "%msbuilddir%msbuild" Compiler.sln /fl1 /p:Configuration=%1		/t:Build /v:m /nologo
 if exist build-%1.log del build-%1.log
--- XSharp/restore.cmd
+++ XSharp/restore.cmd
@@ -1,2 +1,2 @@
 set nugetdir=%userprofile%\.nuget\packages\nuget.commandline\4.6.2\tools\
-%nugetdir%nuget restore master.sln
+%nugetdir%nuget restore Compiler.sln

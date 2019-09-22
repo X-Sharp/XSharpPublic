7de0c2f0aef21f4f8c8ac82fba8736b952f59cf0
Robert van der Hulst <robert@xsharp.eu>
2019-09-22 21:34:02 +0200

    [Doc & Build] Updated

--- XSharp/build.cmd
+++ XSharp/build.cmd
@@ -4,16 +4,16 @@ if /i "%1" == "Public" goto Ok
 if /i "%1" == "Release" goto Ok
 goto Error
 :Ok
-call findmsbuild.cmd
-if "%msbuilddir%" == "" goto NotFound
+rem call findmsbuild.cmd
+if "%VSINSTALLDIR%" == "" goto NotFound
 :found
 set tmpXSharpDev=%XSharpDev%
 rem Reset the XSharpDev path so we will not use the compiler we are generating
 set XSharpDev=
 taskkill  /f /t /fi "IMAGENAME eq XSCompiler.exe" >nul
-Echo Building Compiler %1 Configuration  with Compiler.sln
-Echo Using MsBuild in %msbuilddir%
-"%msbuilddir%msbuild" Compiler.sln /fl1 /p:Configuration=%1		/t:Build /v:m /nologo
+Echo Building Compiler %1 Configuration with Compiler.sln
+Echo Using VS location in %VSINSTALLDIR%
+msbuild Compiler.sln /fl1 /p:Configuration=%1		/t:Build /v:m /nologo
 if exist build-%1.log del build-%1.log
 rename msbuild1.log build-%1.log
 set XSharpDev=%tmpXSharpDev%
@@ -22,6 +22,6 @@ Goto End
 echo Syntax: Build Debug , Build Public or Build Release 
 goto end
 :NotFound
-echo Cannot locate VS 2017 MsBuild.exe
+echo Cannot locate VS 
 goto end
 :End

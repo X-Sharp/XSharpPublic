6423a1c4658428528139ca6d4e0ae1ed3916f679
Robert van der Hulst <robert@xsharp.eu>
2019-10-13 20:35:32 +0200

    [Setup] Installer and header file changes for 2.08

--- XSharp/src/Common/BuildNumber.h
+++ XSharp/src/Common/BuildNumber.h
@@ -11,14 +11,14 @@
     // This generally changes when we release a new full build
     // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE
     #define VERSION_NUMBER_STR     "2.0.0.0"
-    #define VERSION_NUMBER			2,0.0,0
-    #define FILEVERSION_NUMBER       2,0,7,1
-    #define FILEVERSION_NUMBER_STR   "2.0.7.1"
-    #define INFORMATIONAL_NUMBER_STR  "2.07a GA"
+    #define VERSION_NUMBER			2,0,0,0
 
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
+    #define FILEVERSION_NUMBER       2,0,8,0
+    #define FILEVERSION_NUMBER_STR   "2.0.8.0"
+    #define INFORMATIONAL_NUMBER_STR  "2.08 GA"
     #ifdef __DEBUG__
         #define ASSEMBLY_CONFIGURATION "Debug"
     #else

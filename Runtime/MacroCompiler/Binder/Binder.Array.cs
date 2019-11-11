d8b6da05c59e88a8efed17d9c6acf733c070f8ad
Robert van der Hulst <robert@xsharp.eu>
2019-11-11 16:03:07 +0100

    [Macrocompiler & Runtime] Added IMacroCompiler2 that allows to define a delegate to help resolve ambiguous calls. The macro compiler now calls this and passes the ambigous methods/constructors/properties found.
    Also fixed a problem in the error handler for indexed access that was causing an access violation and added casts to Int32 for Array indices that are processed with the "-1" operation

--- Runtime/MacroCompiler/Binder/Binder.Array.cs
+++ Runtime/MacroCompiler/Binder/Binder.Array.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,13 +32,18 @@ namespace XSharp.MacroCompiler
                 for (int i = 0; i < args.Args.Count; i++)
                 {
                     args.Args[i].Expr = BinaryExpr.Bound(args.Args[i].Expr, args.Args[i].Expr.Token, LiteralExpr.Bound(Constant.Create(1)), BinaryOperatorKind.Subtraction, false, Options.Binding);
+                    var expr = args.Args[i].Expr;
+                    Binder.Convert(ref expr, Compilation.Get(NativeType.Int32), BindOptions.Default);
+                    args.Args[i].Expr = expr;
+
                 }
             }
         }
 
-        internal static MemberSymbol TryBindArrayAccess(Expr self, Symbol symbol, ArgList args, ref OverloadResult ovRes, BindOptions options)
+        internal  MemberSymbol TryBindArrayAccess(Expr self, Symbol symbol, ArgList args, ref OverloadResult ovRes, BindOptions options)
         {
             bool isStatic = self == null;
+            var matching = new List<OverloadResult>();
 
             if ((symbol as PropertySymbol)?.IsStatic == isStatic)
             {
@@ -55,6 +60,14 @@ namespace XSharp.MacroCompiler
                         CheckArguments(p as PropertySymbol, (p as PropertySymbol).Parameters, args, ref ovRes, options);
                         if (ovRes?.Exact == true)
                             break;
+                        if (ovRes != null)
+                        {
+                            if (!matching.Contains(ovRes))
+                                matching.Add(ovRes);
+                            if (ovRes.Equivalent != null && !matching.Contains(ovRes.Equivalent))
+                                matching.Add(ovRes.Equivalent);
+                        }
+
                     }
                 }
             }
@@ -64,6 +77,17 @@ namespace XSharp.MacroCompiler
                 ApplyConversions(args, ovRes);
                 return ovRes.Symbol;
             }
+            if (matching.Count > 1 && ovRes.Valid && Options.Resolver != null)
+            {
+                var res = AskUserForCorrectOverload(matching, args);
+                if (res != null)
+                {
+                    ovRes = res;
+                    ApplyConversions(args, ovRes);
+                    return ovRes.Symbol;
+                }
+            }
+
             return null;
         }
     }

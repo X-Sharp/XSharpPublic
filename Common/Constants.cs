b4527dc7acf8e44f0fa00526fb6227f708d65d86
Robert van der Hulst <robert@xsharp.eu>
2019-11-13 07:16:09 +0100

    [Vs] Version 2.0.1.0

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -12,9 +12,9 @@ namespace XSharp
         internal const string ProductName = "XSharp Bandol";
         internal const string Product = "XSharp";
         // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE BUILDNUMBER.H FILE
-        internal const string Version = "2.0.0.0";
-        internal const string FileVersion = "2.0.8.1";
-        internal const string ProductVersion = "2.08a GA";
+        internal const string Version = "2.1.0.0";
+        internal const string FileVersion = "2.1.0.0";
+        internal const string ProductVersion = "2.10 GA";
         internal const string Copyright = "Copyright © XSharp BV 2015-2019";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;

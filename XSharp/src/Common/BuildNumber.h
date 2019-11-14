b58bb7f1159eaa9b6d2b1dccefcfaa8c2bfa0afd
Robert van der Hulst <robert@xsharp.eu>
2019-11-14 12:17:57 +0100

    [Compiler] Version number

--- XSharp/src/Common/BuildNumber.h
+++ XSharp/src/Common/BuildNumber.h
@@ -10,15 +10,15 @@
     // This is used for the assembly version number, which the CLR uses to determine binding
     // This generally changes when we release a new full build
     // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE
-    #define VERSION_NUMBER_STR     "2.0.0.0"
-    #define VERSION_NUMBER			2,0,0,0
+    #define VERSION_NUMBER_STR     "2.1.0.0"
+    #define VERSION_NUMBER			2,1,0,0
 
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-    #define FILEVERSION_NUMBER       2,0,8,1
-    #define FILEVERSION_NUMBER_STR   "2.0.8.1"
-    #define INFORMATIONAL_NUMBER_STR  "2.08a GA"
+    #define FILEVERSION_NUMBER       2,1,0,0
+    #define FILEVERSION_NUMBER_STR   "2.1.0.0"
+    #define INFORMATIONAL_NUMBER_STR  "2.10 GA"
     #ifdef __DEBUG__
         #define ASSEMBLY_CONFIGURATION "Debug"
     #else

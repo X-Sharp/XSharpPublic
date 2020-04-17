2352fbb5f1774508cbeff4b7916af5b0f08d7e45
Robert van der Hulst <robert@xsharp.eu>
2020-04-17 11:15:14 +0200

    [Tools] version for SHFB assemblies is now version of rest of the projects.  Also updated version numbers

---
+++ Tools/SHFB/XSharpSyntax/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XSharp SHFB Syntax component")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(false)]
+

1fe217d277dabad23678d4639ea589c0a27a09d9
Robert van der Hulst <robert@xsharp.eu>
2020-06-24 14:40:34 +0200

    [Compiler] Updated version

--- XSharp/src/Common/Constants.cs
+++ XSharp/src/Common/Constants.cs
@@ -9,12 +9,12 @@ namespace XSharp
     {
         internal const string Company = "XSharpBV";
         internal const string RegCompany = "XSharpBV";
-        internal const string ProductName = "XSharp Bandol";
+        internal const string ProductName = "XSharp Cahors";
         internal const string Product = "XSharp";
         // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE BUILDNUMBER.H FILE
         internal const string Version = "2.1.0.0";
-        internal const string FileVersion = "2.4.1.0";
-        internal const string ProductVersion = "2.4a GA";
+        internal const string FileVersion = "2.5.0.0";
+        internal const string ProductVersion = "2.5 GA";
         internal const string Copyright = "Copyright © XSharp BV 2015-2020";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;

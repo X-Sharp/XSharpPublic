b4752beb88292e024c60175b11d3eefe26d443a1
Robert van der Hulst <robert@xsharp.eu>
2020-07-29 15:54:11 +0200

    [MsBuild] The DLL no longer depends on Tasks.v4.0 and Utilities.v4.0. To do so I have coped some code from the existing DLLs to the CopiedCode folder

---
+++ VisualStudio/XSharp.Build/CopiedCode/TaskExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Tasks;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSharp.Build
+{
+
+    public abstract class TaskExtension : Microsoft.Build.Utilities.Task
+    {
+        private TaskLoggingHelperExtension _logExtension;
+
+        public new TaskLoggingHelper Log => _logExtension;
+
+        internal TaskExtension()
+            : base(Microsoft.Build.Shared.AssemblyResources.PrimaryResources, "MSBuild.")
+        {
+            _logExtension = new TaskLoggingHelperExtension(this, Microsoft.Build.Shared.AssemblyResources.PrimaryResources, Microsoft.Build.Shared.AssemblyResources.SharedResources, "MSBuild.");
+        }
+    }
+}

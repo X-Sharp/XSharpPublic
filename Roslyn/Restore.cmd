6ead3c1bf89ea477299f761c2053ea8510221c06
rolfvdhulst <37153373+rolfvdhulst@users.noreply.github.com>
2020-07-30 14:47:16 +0200

    [CI] revert changes to Restore.cmd

--- Roslyn/Restore.cmd
+++ Roslyn/Restore.cmd
@@ -1,5 +1,2 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
 powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build\scripts\build.ps1" -restore %*
-echo Start building compilers
-msbuild "%~dp0\Compilers.sln" 

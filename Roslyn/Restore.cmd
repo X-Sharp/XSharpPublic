7fbce903bcee4e91fe593060edfd9b0a2bae1762
rolfvdhulst <37153373+rolfvdhulst@users.noreply.github.com>
2020-07-30 14:29:21 +0200

    Add continuous integration (#9)
    * [CI] Attempt to add VS command line tool
    
    * [CI] Try removing buildsteps for testing
    
    * [CI] try Build.cmd instead of Restore
    
    * [CI] attempt to call vscmd before powershell
    
    * [CI] back to restore.cmd
    
    * [CI] attempt building Roslyn
    
    * [CI] reduce amount of build targets
    
    * [CI] fix build error
    
    * [CI] add echo

--- Roslyn/Restore.cmd
+++ Roslyn/Restore.cmd
@@ -1,3 +1,5 @@
 @echo off
-powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build\scripts\build.ps1" -restore %* 
-
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+powershell -noprofile -executionPolicy RemoteSigned -file "%~dp0\build\scripts\build.ps1" -restore %*
+echo Start building compilers
+msbuild "%~dp0\Compilers.sln" 

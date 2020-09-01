9c5e406965d2bd352975e29ee097709d84937402
Robert <robert@xsharp.eu>
2020-09-01 14:54:57 +0200

    [Compiler] Updated <MSBuildAllProjects> property to include the props and targets files. Updated casing of assembly description

--- XSharp/src/Compiler/xsc/Properties/AssemblyInfo.cs
+++ XSharp/src/Compiler/xsc/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XSC.EXE")]
-[assembly: AssemblyDescription("X# commandline compiler")]
+[assembly: AssemblyDescription("X# Commandline Compiler")]

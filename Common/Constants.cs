ee77a0d4d0aeeb49ac26eaa0b89d66e997bd9ee4
Robert van der Hulst <robert@xsharp.eu>
2020-09-24 12:25:02 +0200

    [VsIntegration] Updated file version to 2.6.0.1

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -14,8 +14,8 @@ namespace XSharp
         internal const string Product = "XSharp";
         // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE BUILDNUMBER.H FILE
         internal const string Version = "2.6.0.0";
-        internal const string FileVersion = "2.6.0.0";
-        internal const string ProductVersion = "2.6 GA";
+        internal const string FileVersion = "2.6.0.1";
+        internal const string ProductVersion = "2.6a GA";
         internal const string PublicKey = "ed555a0467764586";
         internal const string Copyright = "Copyright © XSharp BV 2015-2020";
 

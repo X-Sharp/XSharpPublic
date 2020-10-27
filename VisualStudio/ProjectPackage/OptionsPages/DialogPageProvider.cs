67482003e89576a89729f2e7b3f2ef644e47150a
Robert van der Hulst <robert@xsharp.eu>
2020-10-27 14:24:41 +0100

    [VsIntegration] Added option page for Custom XSharp Editors

---
+++ VisualStudio/ProjectPackage/OptionsPages/DialogPageProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace XSharp.Project.Options
+{
+    /// <summary>
+    /// A provider for custom <see cref="DialogPage" /> implementations.
+    /// </summary>
+    internal class DialogPageProvider
+    {
+        public class WindowEditor : BaseOptionPage<WindowEditorOptions> { }
+    }
+}

3e06b4d15378cfb173aa9bc352faca91ba09aa25
Nikos Kokkalis <nikos.v.kokkalis@gmail.com>
2020-10-31 11:21:43 +0200

    [macro] Merge USUAL and OBJECT macro compiler

--- Runtime/MacroCompiler/Compiler/MacroCodeblock.cs
+++ Runtime/MacroCompiler/Compiler/MacroCodeblock.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XSharp.MacroCompiler.ObjectMacro
+﻿namespace XSharp.MacroCompiler.ObjectMacro
 {
     public delegate object MacroCodeblockDelegate(params object[] args);
 
@@ -29,36 +22,42 @@ namespace XSharp.MacroCompiler.ObjectMacro
 
 namespace XSharp.MacroCompiler.UsualMacro
 {
+    using static XSharp.RT.Functions;
     public delegate __Usual MacroCodeblockDelegate(params __Usual[] args);
 
-    public class MacroCodeblock : Codeblock
+    class DummyCodeblock : ICodeblock
+    {
+        int _pcount;
+        public DummyCodeblock(int pCount) { _pcount = pCount; }
+        public object EvalBlock(params object[] args) => null;
+        public int PCount() => _pcount;
+
+    }
+
+    public class MacroCodeblock : _Codeblock
     {
         MacroCodeblockDelegate _eval;
-        string _source;
         public override __Usual Eval(params __Usual[] args) => _eval(args);
-        public MacroCodeblock(MacroCodeblockDelegate evalMethod, int pCount) : this(evalMethod, pCount, null) { }
-        public MacroCodeblock(MacroCodeblockDelegate evalMethod, int pCount, string source) : base(pCount)
+        public MacroCodeblock(MacroCodeblockDelegate evalMethod, int pCount, string source, bool isBlock) : base(new DummyCodeblock(pCount), source, isBlock, false)
         {
             _eval = evalMethod;
-            _source = source;
         }
-        public override string ToString() => _source;
     }
 
     public class MacroMemVarCodeblock : MacroCodeblock
     {
         public override __Usual Eval(params __Usual[] args)
         {
-            int nLevel = XSharp.RT.Functions.__MemVarInit();
+            int nLevel = __MemVarInit();
             try
             {
                 return base.Eval();
             }
             finally
             {
-                XSharp.RT.Functions.__MemVarRelease(nLevel);
+                __MemVarRelease(nLevel);
             }
         }
-        public MacroMemVarCodeblock(MacroCodeblockDelegate evalMethod, int pCount, string source) : base(evalMethod, pCount, source) { }
+        public MacroMemVarCodeblock(MacroCodeblockDelegate evalMethod, int pCount, string source, bool isBlock) : base(evalMethod, pCount, source, isBlock) { }
     }
 }

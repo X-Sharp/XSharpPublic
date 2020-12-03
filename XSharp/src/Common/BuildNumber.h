4aafbc4e5023ea9a32679f960027cf29633a9c24
Robert <robert@xsharp.eu>
2020-12-03 15:52:56 +0100

    [Compiler] Buildnumber 2.7.0.1

--- XSharp/src/Common/BuildNumber.h
+++ XSharp/src/Common/BuildNumber.h
@@ -16,7 +16,7 @@
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-    #define FILEVERSION_NUMBER   "2.7.0.0"
+    #define FILEVERSION_NUMBER   "2.7.0.1"
     #define INFORMATIONAL_NUMBER  "2.7 GA"
 
     #ifdef __DEBUG__

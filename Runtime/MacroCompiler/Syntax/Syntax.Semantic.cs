3c506edfa920ac7e7cec7ea584dfc827e562669c
Nikos Kokkalis <nikos.v.kokkalis@gmail.com>
2021-06-05 19:07:55 +0300

    [macro] Support REF/OUT keywords (related to #653)

---
+++ Runtime/MacroCompiler/Syntax/Syntax.Semantic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSharp.MacroCompiler.Syntax
+{
+    internal enum RefKind
+    {
+        None,
+        Ref,
+        Out,
+    }
+}

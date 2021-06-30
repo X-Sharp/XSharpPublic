e8afd09360ba7c9ef7a7f8ca532dc56c75b5ec75
Robert van der Hulst <robert@xsharp.eu>
2021-06-30 15:45:08 +0200

    [VsIntegration] Version Number updated

--- Common/BuildNumber.h
+++ Common/BuildNumber.h
@@ -19,8 +19,8 @@
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-    #define FILEVERSION_NUMBER   "2.8.2.13"
-    #define INFORMATIONAL_NUMBER  "2.8b GA"
+    #define FILEVERSION_NUMBER   "2.8.3.14"
+    #define INFORMATIONAL_NUMBER  "2.8c GA"
 
     #ifdef __DEBUG__
         #define ASSEMBLY_CONFIGURATION "Debug"

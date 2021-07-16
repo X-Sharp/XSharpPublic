4ec2b8bbd118ce4ce96477537c5d0d9b4c6be2ad
Robert <robert@xsharp.eu>
2021-07-16 17:32:38 +0200

    [Compiler] Updated build number to 2.8.4.1

--- XSharp/src/Common/Constants.cs
+++ XSharp/src/Common/Constants.cs
@@ -18,8 +18,8 @@ namespace XSharp
 #else
         internal const string Version = "2.8.0.0";
 #endif
-        internal const string FileVersion = "2.8.3.15";
-        internal const string ProductVersion = "2.8c GA";
+        internal const string FileVersion = "2.8.4.1";
+        internal const string ProductVersion = "2.8d GA";
         internal const string PublicKey = "ed555a0467764586";
         internal const string Copyright = "Copyright © XSharp BV 2015-2021";
 

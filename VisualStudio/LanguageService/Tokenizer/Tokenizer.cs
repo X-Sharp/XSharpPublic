69e6c5edf4a3bf87d9a70736eae2615cb8891ee4
Robert van der Hulst <robert@xsharp.eu>
2021-07-21 18:08:21 +0200

    [VsIntegration] Documented some of the functionality

--- VisualStudio/LanguageService/Tokenizer/Tokenizer.cs
+++ VisualStudio/LanguageService/Tokenizer/Tokenizer.cs
@@ -12,19 +12,7 @@ using static XSharp.Parser.VsParser;
 
 namespace XSharp.LanguageService
 {
-    [Flags]
-    public enum CompletionState
-    {
-        None = 0,
-        General = 1 << 0,
-        Namespaces = 1 << 1,
-        Types = 1 << 2,
-        Interfaces = 1 << 3,
-        StaticMembers = 1 << 4,
-        InstanceMembers = 1 << 5,
-        Constructors = 1 << 6,
-        Brackets = 1 << 7,
-    }
+    
     /// <summary>
     /// Static class Tools. Offer services to get TokenList, Search members, ...
     /// </summary>

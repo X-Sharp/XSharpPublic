b2c0f5d4294a190e8e3e1ef2671914afc1100f0e
Robert van der Hulst <robert@xsharp.eu>
2021-07-21 18:08:21 +0200

    [VsIntegration] Documented some of the functionality

---
+++ VisualStudio/LanguageService/Completion/CompletionState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSharp.LanguageService
+{
+    [Flags]
+    public enum CompletionState
+    {
+        None = 0,
+        General = 1 << 0,
+        Namespaces = 1 << 1,
+        Types = 1 << 2,
+        Interfaces = 1 << 3,
+        StaticMembers = 1 << 4,
+        InstanceMembers = 1 << 5,
+        Constructors = 1 << 6,
+        Brackets = 1 << 7,
+        Keywords=1 << 8,
+        Snippets=1 << 9,
+    }
+}

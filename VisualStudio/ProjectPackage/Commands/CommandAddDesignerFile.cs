4edafd10e9871d9a619474a4ef18c6c68683e83b
Robert van der Hulst <robert@xsharp.eu>
2021-08-18 18:11:18 +0200

    [VsIntegration] Added skeleton for Add designer file

--- VisualStudio/ProjectPackage/Commands/CommandAddDesignerFile.cs
+++ VisualStudio/ProjectPackage/Commands/CommandAddDesignerFile.cs
@@ -11,15 +11,17 @@ namespace XSharp.Project
     [Command(PackageIds.idAddDesignerFile)]
     internal sealed class CommandAddDesignerFile : BaseCommand<CommandAddDesignerFile>
     {
+        File currentFile = null;
         protected override void BeforeQueryStatus(EventArgs e)
         {
             base.BeforeQueryStatus(e);
+            currentFile = null;
             ThreadHelper.JoinableTaskFactory.Run(CheckAvailabilityAsync);
         }
         private async Task CheckAvailabilityAsync()
         {
             var items = await VS.Solutions.GetActiveItemsAsync();
-            
+
             bool visible = false;
             foreach (var item in items)
             {
@@ -29,11 +31,13 @@ namespace XSharp.Project
                     if (subtype == ProjectFileAttributeValue.Form || subtype == ProjectFileAttributeValue.UserControl)
                     {
                         visible = true;
+                        currentFile = file;
                         foreach (var child in file.Children)
                         {
                             if (child.FullPath.ToLower().EndsWith(".designer.prg"))
                             {
                                 visible = false;
+                                currentFile = null;
                             }
                         }
                     }
@@ -49,7 +53,22 @@ namespace XSharp.Project
 
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            await VS.MessageBox.ShowAsync("Not ready yet");
+            if (currentFile != null)
+            {
+                var newfile = System.IO.Path.ChangeExtension(currentFile.FullPath, ".designer.prg");
+                await VS.StatusBar.ShowMessageAsync("Creating designer file:" + newfile);
+                System.IO.File.WriteAllText(newfile, "");
+                var project = currentFile.ContainingProject;
+                if (project != null)
+                {
+                    var xproject = XSolution.FindProject(project.FullPath);
+                    if (xproject != null)
+                    {
+                        xproject.ProjectNode.AddFileNode(newfile);
+                    }
+                }
+                await VS.StatusBar.ShowMessageAsync("");
+            }
         }
     }
 }

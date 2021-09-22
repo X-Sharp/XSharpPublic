faa294eff1715bee72947731e0c658c6c63cf044
Robert van der Hulst <robert@xsharp.eu>
2021-09-22 16:53:12 +0200

    Uodated common files

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -16,28 +16,16 @@ namespace XSharp
 #if RUNTIME
         internal const string Version = "2.6.0.0";
 #else
-        internal const string Version = "2.8.0.0";
+        internal const string Version = "2.9.0.0";
 #endif
-        internal const string FileVersion = "2.8.4.1";
-        internal const string ProductVersion = "2.8d GA";
+        internal const string FileVersion = "2.9.0.0";
+        internal const string ProductVersion = "2.9 GA";
         internal const string PublicKey = "ed555a0467764586";
         internal const string Copyright = "Copyright © XSharp BV 2015-2021";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;
         internal const string RegistryKey64 = @"Software\WOW6432Node\" + RegCompany + @"\" + Product;
         internal const string RegistryValue = "XSharpPath";
-
-        // Names of registry keys that contain settings used by code generation in the Code Generators
-        // These are stored in the registry because the CodeDomProvider is also called "stand alone" from the
-        // build process when generating code for XAML files
-        internal const string RegistryKeywordCase = "KeywordCase";
-        internal const string RegistryPrivateKeyword = "PrivateKeyword";
-        internal const string RegistryPublicKeyword = "PublicKeyword";
-        internal const string RegistryUseTabs = "UseTabs";
-        internal const string RegistryTabSize = "TabSize";
-        internal const string RegistryIndentSize = "IndentSize";
-        internal static bool optionWasChanged = false;
-
 
         // Environment variable that points to the BIN folder where xsc.exe AND rc.exe are located
         internal const string EnvironmentXSharpBin = "XSHARPBINPATH";
@@ -46,51 +34,5 @@ namespace XSharp
         // This also enables the "magic" button on the tools-options dialog
         // and adds CRLF to the response file between the various commands to make it easier to read.
         internal const string EnvironmentXSharpDev = "XSHARPDEV";
-
-        // Read Setting from Registry
-        internal static bool WriteSetting(string name, int defvalue)
-        {
-            object result = defvalue;
-            try
-            {
-                var key = Microsoft.Win32.Registry.CurrentUser;
-                var subkey = key.OpenSubKey(Constants.RegistryKey, true);
-                if (subkey == null)
-                {
-                    subkey = key.CreateSubKey(Constants.RegistryKey, true);
-                }
-                subkey.SetValue(name, defvalue);
-                return true;
-            }
-            catch
-            {
-            }
-            return false;
-        }
-        // Write Setting from Registry
-        internal static object GetSetting(string name, int defvalue)
-        {
-            object result = defvalue;
-            try
-            {
-                var key = Microsoft.Win32.Registry.CurrentUser;
-                var subkey = key.OpenSubKey(Constants.RegistryKey, true);
-                if (subkey == null)
-                {
-                    subkey = key.CreateSubKey(Constants.RegistryKey, true);
-                }
-                result = subkey.GetValue(name);
-                if (result == null)
-                {
-                    subkey.SetValue(name, defvalue);
-                    result = defvalue;
-                }
-            }
-            catch
-            {
-                result = defvalue;
-            }
-            return result;
-        }
     }
 }

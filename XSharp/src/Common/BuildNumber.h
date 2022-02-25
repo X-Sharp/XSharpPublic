73de3ea2f9de7d516e091cbffc2a24fe15fcd28d
Robert <robert@xsharp.eu>
2022-02-25 17:01:13 +0100

    Version 2.11

--- XSharp/src/Common/BuildNumber.h
+++ XSharp/src/Common/BuildNumber.h
@@ -13,14 +13,14 @@
 #ifdef RUNTIME
     #define VERSION_NUMBER     "2.6.0.0"
 #else
-    #define VERSION_NUMBER     "2.10.1.0"
+    #define VERSION_NUMBER     "2.11.0.0"
 #endif
 
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-    #define FILEVERSION_NUMBER   "2.10.1.0"
-    #define INFORMATIONAL_NUMBER  "2.10a GA"
+    #define FILEVERSION_NUMBER   "2.11.0.0"
+    #define INFORMATIONAL_NUMBER  "2.11 GA"
 
     #ifdef __DEBUG__
         #define ASSEMBLY_CONFIGURATION "Debug"

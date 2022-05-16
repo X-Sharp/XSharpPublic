72e599651f8d391ad2128524042af68a52efb1bf
Robert van der Hulst <robert@xsharp.eu>
2022-05-16 13:39:25 +0200

    Generate the package def for the languageservice in the post build event

--- VisualStudio/LanguageService/generatepkgdef.cmd
+++ VisualStudio/LanguageService/generatepkgdef.cmd
@@ -1,5 +1,9 @@
-rem If the build process fails to generate the Languageservice PkgDef file call:
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VSSDK\VisualStudioIntegration\Tools\Bin\CreatePkgDef.exe" c:\XSharp\DevPublic\Binaries\2019\Debug\XSharpLanguageService.dll  /out=c:\XSharp\DevPublic\Binaries\2019\Debug\XSharpLanguageService.pkgdef /verbose /codebase
-copy c:\XSharp\DevPublic\Binaries\2019\Debug\XSharpLanguageService.pkgdef  XSharpLanguageService.pkgdef
-copy c:\XSharp\DevPublic\Binaries\2019\Debug\XSharpLanguageService.pkgdef  ..\ProjectPackage\XSharpLanguageService.pkgdef
-pause
+rem use this cmd file to generate the pkgdef with the VS2017 tools
+set vsdir=c:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise
+set langservicedir=%1
+set currentdir=%cd%
+rem echo The langservicedir is: %langservicedir%
+rem echo The currentdir is: %currentdir%
+"%vsdir%\VSSDK\VisualStudioIntegration\Tools\Bin\CreatePkgDef.exe" c:\XSharp\DevPublic\Binaries\2019\Debug\XSharpLanguageService.dll  /out=c:\XSharp\DevPublic\Binaries\2019\Debug\XSharpLanguageService.pkgdef /verbose /codebase
+copy %cd%\XSharpLanguageService.pkgdef  %1XSharpLanguageService.pkgdef
+copy %cd%\XSharpLanguageService.pkgdef  %1..\ProjectPackage\XSharpLanguageService.pkgdef

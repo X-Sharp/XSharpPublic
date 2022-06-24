ccffcb21103557e609c9da779dc2b5be0e5705fb
Robert van der Hulst <robert@xsharp.eu>
2022-06-24 18:17:36 +0200

    [VSIntegration] Updated version numbers

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -16,10 +16,10 @@ namespace XSharp
 #if RUNTIME
         internal const string Version = "2.6.0.0";
 #else
-        internal const string Version = "2.12.2.0";
+        internal const string Version = "2.13.0.0";
 #endif
-        internal const string FileVersion = "2.12.2.0";
-        internal const string ProductVersion = "2.12 GA";
+        internal const string FileVersion = "2.13.0.0";
+        internal const string ProductVersion = "2.13 GA";
         internal const string PublicKey = "ed555a0467764586";
         internal const string Copyright = "Copyright © XSharp BV 2015-2022";
 

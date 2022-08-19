f4b2832651ffc08d8c8503fe003fb5d8f1dba61d
Robert van der Hulst <robert@xsharp.eu>
2022-08-19 21:08:09 +0200

    2.13.0.7

--- Common/BuildNumber.h
+++ Common/BuildNumber.h
@@ -13,13 +13,13 @@
 #ifdef RUNTIME
     #define VERSION_NUMBER     "2.6.0.0"
 #else
-    #define VERSION_NUMBER     "2.13.0.6"
+    #define VERSION_NUMBER     "2.13.0.7"
 #endif
 
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-    #define FILEVERSION_NUMBER   "2.13.0.6"
+    #define FILEVERSION_NUMBER   "2.13.0.7"
     #define INFORMATIONAL_NUMBER  "2.13 GA"
 
     #ifdef __DEBUG__

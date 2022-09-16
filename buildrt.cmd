7a686ea051a40d51e20a5d1503804d6b2f8f6008
Robert van der Hulst <robert@xsharp.eu>
2022-09-16 10:07:18 +0200

    Build RT with VS2022

--- buildrt.cmd
+++ buildrt.cmd
@@ -2,7 +2,7 @@
 if "%VCINSTALLDIR"== "" goto setVsPath
 goto rest
 :setVsPath
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
 :rest
 if "%libpath%" == "" goto NotFound
 if /i "%1" == "Debug" goto Ok

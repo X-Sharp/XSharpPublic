d4ee48ff640aa8dff3504b626ae711e5c41cf02d
Robert van der Hulst <robert@xsharp.eu>
2022-10-19 09:47:26 +0200

    Removed unneeded references

--- VisualStudio/ProjectPackage/Editors/XSharpProjectFactory.cs
+++ VisualStudio/ProjectPackage/Editors/XSharpProjectFactory.cs
@@ -8,7 +8,6 @@ using Microsoft.VisualStudio.Project;
 using System;
 using System.Runtime.InteropServices;
 using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
-using XSharp.LanguageService;
 using Microsoft.VisualStudio.Shell;
 
 namespace XSharp.Project

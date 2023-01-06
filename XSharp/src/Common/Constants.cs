8fd3c893fe809ec8a3d495a73929a3af2386a3cc
Robert van der Hulst <robert@xsharp.eu>
2023-01-06 17:15:50 +0100

    [Compiler] Updated version number to 2.15

--- XSharp/src/Common/Constants.cs
+++ XSharp/src/Common/Constants.cs
@@ -16,12 +16,12 @@ namespace XSharp
 #if RUNTIME
         internal const string Version = "2.6.0.0";
 #else
-        internal const string Version = "2.14.0.4";
+        internal const string Version = "2.15.0.1";
 #endif
-        internal const string FileVersion = "2.14.0.4";
-        internal const string ProductVersion = "2.14 GA";
+        internal const string FileVersion = "2.15.0.1";
+        internal const string ProductVersion = "2.15 GA";
         internal const string PublicKey = "ed555a0467764586";
-        internal const string Copyright = "Copyright © XSharp BV 2015-2022";
+        internal const string Copyright = "Copyright © XSharp BV 2015-2023";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;
         internal const string RegistryKey64 = @"Software\WOW6432Node\" + RegCompany + @"\" + Product;

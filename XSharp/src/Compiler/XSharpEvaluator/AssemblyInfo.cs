1a56fadfcf4f86723b6062b5f210620b917a4b7c
RobertvanderHulst <robert@xsharp.eu>
2023-01-18 15:43:18 +0100

    [ExpressionEvaluator] Removed duplicate code in the XSharp branch of the tree and used the original code. On several places the Resources class is renamed to XResources to resolve to our Resources type Use the CommonAssembly info for common Assembly properties

--- XSharp/src/Compiler/XSharpEvaluator/AssemblyInfo.cs
+++ XSharp/src/Compiler/XSharpEvaluator/AssemblyInfo.cs
@@ -5,14 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LanguageService.CodeAnalysis.ExpressionEvaluator")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LanguageService.CodeAnalysis.ExpressionEvaluator")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTitle("XSharp.ExpressionEvaluator")]
+[assembly: AssemblyDescription("XSharp ExpressionEvaluator, based on the Roslyn Expression Evaluator")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,15 +16,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d57f9cf0-87aa-4c91-8c0b-c25d1696985e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("XSharpDebugger2022,PublicKey=0024000004800000940000000602000000240000525341310004000001000100b16a35b62bb33ce476c595e75bcc83fe4566c0a7cb9c093ce23e7add61fe1fc8a6edca2e542f0dc9ce41ec6b4260a73dda598c81f61a6f9522653ebfeae098a3bdb641020e843cbab825afe1c3910d42d17a1dcf211abb1cba4fc5e19569307c67a11c92b848d2df23f454d5ed1ab8b479afa4ece799445292b11012225aee96")]
+

46d260a16c4f49cd77f600dec14267fb07288758
RobertvanderHulst <robert@xsharp.eu>
2023-01-30 17:17:15 +0100

    [Vsintegration] Fixes #1153

--- VisualStudio/LanguageService/Formatting/LineFormatter.cs
+++ VisualStudio/LanguageService/Formatting/LineFormatter.cs
@@ -352,10 +352,12 @@ namespace XSharp.LanguageService
             }
             else if (prevLineKeyword.IsEntity())
             {
+                // inside a type we may want to indent members
                 if (prevLineKeyword.IsType() && settings.IndentTypeMembers)
                 {
                     indentValue += settings.IndentSize;
                 }
+                // inside a member we may want to indent statements
                 else if (prevLineKeyword.IsMember() && settings.IndentStatements)
                 {
                     indentValue += settings.IndentSize;
@@ -363,25 +365,32 @@ namespace XSharp.LanguageService
             }
             else if (prevLineKeyword.IsStart() || prevLineKeyword.IsMiddle())
             {
+                // after a start or middle keyword we increment the indentation
                 indentValue += settings.IndentSize;
-
+                // Namespace is an exception. This depends on a setting
                 if (prevLineKeyword.Kw2 == XTokenType.Namespace && !settings.IndentNamespace)
                 {
+                    
                     indentValue -= settings.IndentSize;
                 }
                 // Check to see if we need to adjust CASE / OTHERWISE
                 if (prevLineKeyword.Kw1 == XTokenType.Case ||
                     prevLineKeyword.Kw1 == XTokenType.Otherwise)
                 {
-
                     if (!settings.IndentCaseContent)
                     {
                         indentValue -= settings.IndentSize;
                     }
                 }
             }
+            else if (prevLineKeyword.IsStop())
+            {
+                // After an end keyword we copy its indentation
+                return indentValue;
+            }
             else if (line > 0)
             {
+                // Read the previous line until we find something that we can use
                 return GetDesiredIndentationAfterLine(line - 1);
             }
             return indentValue;

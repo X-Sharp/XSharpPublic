05a1fd2003a6586bf4efa2bb5fa36863d51b6eca
RobertvanderHulst <robert@xsharp.eu>
2023-01-31 17:34:18 +0100

    [VsIntegration] Updated copyright year

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -21,7 +21,7 @@ namespace XSharp
         internal const string FileVersion = "2.15.0.1";
         internal const string ProductVersion = "2.15 GA";
         internal const string PublicKey = "ed555a0467764586";
-        internal const string Copyright = "Copyright © XSharp BV 2015-2022";
+        internal const string Copyright = "Copyright © XSharp BV 2015-2023";
 
         internal const string RegistryKey = @"Software\" + RegCompany + @"\" + Product;
         internal const string RegistryKey64 = @"Software\WOW6432Node\" + RegCompany + @"\" + Product;

36f43d532583c952eef7710ede8d3a57f2df2cd1
Robert van der Hulst <robert@xsharp.eu>
2023-05-09 23:45:08 +0200

    [Runtime] 2.16.0.2

--- Common/BuildNumber.h
+++ Common/BuildNumber.h
@@ -7,19 +7,21 @@
     #define COMPANY_NAME     "XSharp BV"
     #define REG_COMPANY_NAME  "XSharpBV"
 
+    // This is the file version number, which is ignored by .NET but used by Windows installer to determine
+    // whether one file is newer than another.
+    // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
+#define FILEVERSION_NUMBER   "2.16.0.2"
+
+
     // This is used for the assembly version number, which the CLR uses to determine binding
     // This generally changes when we release a new full build
     // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE and the Versions.Props file
 #ifdef RUNTIME
     #define VERSION_NUMBER     "2.6.0.0"
 #else
-    #define VERSION_NUMBER     "2.16.0.1"
+    #define VERSION_NUMBER     FILEVERSION_NUMBER
 #endif
 
-    // This is the file version number, which is ignored by .NET but used by Windows installer to determine
-    // whether one file is newer than another.
-    // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-    #define FILEVERSION_NUMBER   "2.16.0.1"
     #define INFORMATIONAL_NUMBER  "2.16 GA"
 
     #ifdef __DEBUG__

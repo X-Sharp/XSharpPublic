3986e576d465e90b9c1ec7505ccf3b6b9916c5bc
Robert van der Hulst <robert@xsharp.eu>
2023-06-27 13:42:44 +0200

    [Compiler] Fixes #X-Sharp/XSharpPublic/issues/1285

--- XSharp/src/Compiler/XSharpCodeAnalysis/Binder/Binder_Expressions.cs
+++ XSharp/src/Compiler/XSharpCodeAnalysis/Binder/Binder_Expressions.cs
@@ -452,8 +452,11 @@ namespace Microsoft.CodeAnalysis.CSharp
                 string propName = right.Identifier.ValueText;
                 if (leftType is { })
                 {
+                    // when a class inherits from Object then SUPER:Foo() should not generate a late bind call.
+                    // See https://github.com/X-Sharp/XSharpPublic/issues/1285
+                    bool isSuper = (boundLeft.Kind == BoundKind.BaseReference;
                     bool earlyBound = propName == ".ctor";
-                    bool isObject = leftType.IsObjectType();
+                    bool isObject = leftType.IsObjectType() && !isSuper;
                     bool isUsual = false;
                     bool isArray = false;
                     NamedTypeSymbol usualType = Compilation.UsualType();

d99b43557334defb545858c2152286c68cb541af
Robert van der Hulst <robert@xsharp.eu>
2024-01-04 13:00:20 +0100

    [Compiler] Version 2.19

--- src/Common/BuildNumber.h
+++ src/Common/BuildNumber.h
@@ -3,14 +3,14 @@
     #define BUILDNUMBER_H
     #define PRODUCT_NAME     "XSharp Cahors"
     #define PRODUCT			 "XSharp"
-    #define COPYRIGHT_STR    "Copyright (c) XSharp BV 2015-2023."
+    #define COPYRIGHT_STR    "Copyright (c) XSharp BV 2015-2024."
     #define COMPANY_NAME     "XSharp BV"
     #define REG_COMPANY_NAME  "XSharpBV"
 
     // This is the file version number, which is ignored by .NET but used by Windows installer to determine
     // whether one file is newer than another.
     // This typically would change if we're generating a patch, otherwise it should be the same as VERSION_NUMBER
-#define FILEVERSION_NUMBER   "2.18.1.0"
+#define FILEVERSION_NUMBER   "2.19.0.0"
     // This is used for the assembly version number, which the CLR uses to determine binding
     // This generally changes when we release a new full build
     // NOTE: DO NOT FORGET THE VERSION NUMBER IN THE CONSTANTS.CS FILE and the Versions.Props file
@@ -20,7 +20,7 @@
     #define VERSION_NUMBER     FILEVERSION_NUMBER
 #endif
 
-    #define INFORMATIONAL_NUMBER  "2.18 GA"
+    #define INFORMATIONAL_NUMBER  "2.19 GA"
 
     #ifdef __DEBUG__
         #define ASSEMBLY_CONFIGURATION "Debug"

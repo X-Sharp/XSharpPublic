7509682d734792cb878e3ceb8fd55938d5e8234b
Robert van der Hulst <robert@xsharp.eu>
2024-05-24 13:38:58 +0200

    [VsIntegration] Added Chinese Help

---
+++ src/VisualStudio/ProjectPackage/Commands/CommandXsHelpChinese.cs
@@ -0,0 +1,16 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace XSharp.Project
+{
+    [Command(PackageIds.idHelpOffLineChinese)]
+    internal sealed class CommandXsHelpChinese : BaseCommand<CommandXsHelpChinese>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            string docPath = Commands.GetXsPath(@"Help\XSharp_ZH-CN.chm");
+            await Commands.StartProcessAsync(docPath);
+        }
+    }
+}

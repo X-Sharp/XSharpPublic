6d14b946925b9152820348a0f531298d8d974a6c
Robert van der Hulst <robert@xsharp.eu>
2024-10-01 13:51:19 +0200

    [Vsintegration] Fixes #157-

--- src/VisualStudio/LanguageService/Parameter Info/SignatureCommandHandler.cs
+++ src/VisualStudio/LanguageService/Parameter Info/SignatureCommandHandler.cs
@@ -184,13 +184,15 @@ namespace XSharp.LanguageService
                                     break;
 
                             }
+                            _doc.SignatureStarting = false;
                             break;
-                        case (int)VSConstants.VSStd2KCmdID.RETURN:
+                        case (int)VSConstants.VSStd2KCmdID.RETURN when ! _doc.SignatureStarting:
                             if (_doc.CompletionSession == null)
                             {
                                 CancelSignatureSession();
                             }
                             break;
+
 
                         case (int)VSConstants.VSStd2KCmdID.LEFT:
                         case (int)VSConstants.VSStd2KCmdID.RIGHT:
@@ -227,7 +229,7 @@ namespace XSharp.LanguageService
             var level = 0;
             bool done = false;
             bool inString = false;
-            char closechar = '\0';
+            char closeChar = '\0';
             int pos = ssp.Position;
             int curLine = ssp.GetContainingLine().LineNumber;
             while (!done && pos > 0)
@@ -255,12 +257,12 @@ namespace XSharp.LanguageService
                             if (!inString)
                             {
                                 inString = true;
-                                closechar = ch;
+                                closeChar = ch;
                             }
-                            else if (closechar == ch)
+                            else if (closeChar == ch)
                             {
                                 inString = false;
-                                closechar = '\0';
+                                closeChar = '\0';
                             }
                             break;
                         case '(' when ! inString:
@@ -300,7 +302,7 @@ namespace XSharp.LanguageService
             // Check if we can get the member where we are
 
             var tokenList = XSharpTokenTools.GetTokenListBeforeCaret(location, out var state);
-            // tokenlist may look like ID1 ( ID2 ( e1 , e2)
+            // tokenList may look like ID1 ( ID2 ( e1 , e2)
             // after the closing paren we come here and want to completely remove the  ID2 ( e1, e2 ) part
             // when we have this ID1 ( e1 ) then there should be no parameter completion at all
             // The same for ID1 { e1 }
@@ -368,7 +370,7 @@ namespace XSharp.LanguageService
             }
             if (comma || command)
             {
-                // check to see if there is a lparen or lcurly before the comma
+                // check to see if there is a LPAREN or LCURLY before the comma
                 bool done = false;
                 nested = 0;
                 while (tokenList.Count > 0 && !done)
@@ -462,9 +464,9 @@ namespace XSharp.LanguageService
 
             var props = new XSharpSignatureProperties(location);
             props.triggerChar = triggerchar;
-            var bufpos = this._textView.Caret.Position.BufferPosition;
-            props.triggerPosition = bufpos.Position;
-            props.triggerLine = bufpos.GetContainingLine().LineNumber;
+            var bufferPos = this._textView.Caret.Position.BufferPosition;
+            props.triggerPosition = bufferPos.Position;
+            props.triggerLine = bufferPos.GetContainingLine().LineNumber;
             props.Visibility = Modifiers.Public;
             if (type != null)
             {
@@ -530,7 +532,7 @@ namespace XSharp.LanguageService
                 return false;
 
             SnapshotPoint caret = _textView.Caret.Position.BufferPosition;
-            ITextSnapshot caretsnapshot = caret.Snapshot;
+            ITextSnapshot caretSnapshot = caret.Snapshot;
             //
             if (_signatureBroker.IsSignatureHelpActive(_textView))
             {
@@ -538,7 +540,7 @@ namespace XSharp.LanguageService
             }
             else
             {
-                _signatureSession = _signatureBroker.CreateSignatureHelpSession(_textView, caretsnapshot.CreateTrackingPoint(caret, PointTrackingMode.Positive), false);
+                _signatureSession = _signatureBroker.CreateSignatureHelpSession(_textView, caretSnapshot.CreateTrackingPoint(caret, PointTrackingMode.Positive), false);
             }
             _signatureSession.Properties[typeof(XSharpSignatureProperties)] = props;
 

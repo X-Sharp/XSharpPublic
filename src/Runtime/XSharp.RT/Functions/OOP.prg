X-Sharp/XSharpPublic

commit 4b3113e3fcd9c458c3f21691d77b49388414fda3

[Runtime] Fix conversion error for latebound call with default parameters (#1684)

src/Runtime/XSharp.RT/Functions/OOP.prg
@@ -562,7 +562,7 @@ internal static class OOPHelpers
                     result := oDefAttrib:Value
                     // for usuals there is no need to convert.
                     if oPar:ParameterType != typeof(usual)
-                        result := Convert.ChangeType(result,oPar:ParameterType)
+                        result := OOPHelpers.ValueConvert(result, oPar:ParameterType)
                     endif
                 end switch
             end if

src/Runtime/XSharp.RT/Functions/Set.prg
@@ -128,7 +128,9 @@ FUNCTION Set(nDefine, newValue) AS USUAL CLIPPER
                 state:Settings[nSetting] := cNew:ToUpper() == "ON"
             ELSE
                 TRY
-                    oNew := System.Convert.ChangeType( oNew, oOld:GetType())
+                    IF oNew:GetType() != oOld:GetType()
+                        oNew := OOPHelpers.ValueConvert(oNew, oOld:GetType())
+                    ENDIF
                     state:Settings[nSetting] := oNew
                 CATCH
                     NOP // can't convert, so ignore assignment

commit 4ca12f51467ed59132736d5bf049f9dede6cdeba

[Runtime] Fix for issue with properties that are different in case (#1692). Also fix for issue where Set() is called with a string instead of a numeric value, introduced in commit 4b3113e3fcd9c458c3f21691d77b49388414fda3

src/Runtime/XSharp.RT/Functions/OOP.prg
@@ -605,7 +605,8 @@ internal static class OOPHelpers
         return t:GetMember(cName, memberType, flags)
 
     static method GetField(t as System.Type, cName as string, lInstance as LOGIC, lSelf as LOGIC) as FieldInfo
-        foreach var m in GetMembers(t, cName, MemberTypes.Field,lInstance, lSelf)
+        var members := GetMembers(t, cName, MemberTypes.Field,lInstance, lSelf)
+        foreach var m in members
             if m is FieldInfo var fldInfo
                 return fldInfo
             endif
@@ -613,7 +614,8 @@ internal static class OOPHelpers
         return null
 
     static method GetProperty(t as System.Type, cName as string, lInstance as LOGIC, lSelf as LOGIC) as PropertyInfo
-        foreach var m in GetMembers(t, cName, MemberTypes.Property,lInstance, lSelf)
+        var members := GetMembers(t, cName, MemberTypes.Property,lInstance, lSelf)
+        foreach var m in members
             if m is PropertyInfo var propInfo
                 return propInfo
             endif
@@ -637,17 +639,21 @@ internal static class OOPHelpers
                 if (atts:Length > 0)
                     return 1U
                 endif
-                return 0U
             endif
+            return 0U
         endif
 
         do while t != null
             var pi :=  OOPHelpers.GetProperty(t, cName, true, true)
-            if pi != null .and. ( (lGet .and. pi:CanRead) .or. (.not. lGet .and. pi:CanWrite) )
-                return 3U
-            else
-                t := t:BaseType
+            if pi != null
+                if lGet .and. pi:CanRead
+                    return 3U
+                endif
+                if ! lGet .and. pi:CanWrite
+                    return 3U
+                endif
             endif
+            t := t:BaseType
         enddo
 
         return 0U
@@ -1288,22 +1294,22 @@ internal static class OOPHelpers
                     try
                         return oOperator:Invoke(null, <object>{oValue})
                     catch
-                        local ex as Error
-                        ex := Error{Gencode.EG_WRONGCLASS, "", "Could not convert value "+oValue:ToString() + " to type " + toType:Name}
-                        ex:FuncSym := __function__
-                        ex:Stack := ErrorStack()
-                        throw ex
-
+                        // do not throw error here. We will try to convert the value below with Convert.ChangeType
                     end try
                 endif
             endif
             // when we get here then there is no operator and we will try to change the type..
+            // or the call to the operator failed
             local oRet as object
             try
                 oRet := uValue
                 oRet := Convert.ChangeType(oRet, toType)
             catch
-                oRet := uValue
+                local ex as Error
+                ex := Error{Gencode.EG_WRONGCLASS, "", "Could not convert value "+oValue:ToString() + " to type " + toType:Name}
+                ex:FuncSym := __function__
+                ex:Stack := ErrorStack()
+                throw ex
             end try
             return oRet
         endif

src/Runtime/XSharp.VO.Tests/OOPTests.prg
@@ -578,6 +578,17 @@ BEGIN NAMESPACE XSharp.VO.Tests
 			EXPORT symUsual := #SymUsual AS USUAL
 		END CLASS
 
+        [Fact, Trait("Category", "OOP")];
+        METHOD IVarInfoTest() AS VOID
+            local test := xxTest{} as usual
+            Assert.True(IVarGetInfo(test, #symVerkauf) == 3)
+            Assert.True(IVarPutInfo(test, #symVerkauf) == 3)
+            Assert.True(IVarGetInfo(test, #xxIVar) == 2)
+            Assert.True(IVarPutInfo(test, #xxIVar) == 2)
+            Assert.True(IVarGetInfo(test, #xxInstance) == 1)
+            Assert.True(IVarPutInfo(test, #xxInstance) == 1)
+            Assert.True(IVarGetInfo(test, #xxPrivate) == 0)
+            Assert.True(IVarPutInfo(test, #xxPrivate) == 0)
 
         [Fact, Trait("Category", "OOP")];
 		METHOD IVarsLateBound() AS VOID
@@ -797,3 +808,20 @@ CLASS OOpVisTestClass
 
 
 END CLASS
+public class xxTestBase
+	virtual public access SymVerkauf as symbol
+		return null_symbol
+
+	virtual public assign SymVerkauf(val as symbol) as void
+		nop()
+end class
+
+public class xxTest inherit xxTestBase
+    // difference in case with parent access/assign
+    // This results in the Type.GetMembers() to return more than one property
+    public xxIVar as INT
+    instance xxInstance as INT
+    private xxPrivate as INT
+	public override assign symVerkauf(val as symbol) as void
+		nop()
+end class

src/Runtime/XSharp.RT.Tests/ConversionTests.prg
@@ -418,6 +418,10 @@ BEGIN NAMESPACE XSharp.RT.Tests
             var dec1 := Set(Set.Decimals)
             Assert.True( dec1 is Long)
             Assert.True( dec1 == 1L)
+            Set(Set.Decimals, "3")
+            dec1 := Set(Set.Decimals)
+            Assert.True( dec1 is Long)
+            Assert.True( dec1 == 3L)
             Set(Set.Decimals, 2.0)
             var dec2 := Set(Set.Decimals)
             Assert.True( dec2 is Long)
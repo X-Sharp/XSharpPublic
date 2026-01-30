352aaeccc2dab0016c73b23ef6e067f379d9eb43
Robert van der Hulst <robert@xsharp.eu>
2026-01-30 16:50:14 +0100

    [Nuget] Added package for typed VOSDK. Added dependencies in packages Added content files for VOSDK Deploy VOSDK only for net46 Deploy Script to output folder

--- src/Runtime/Nuget/createAllNugetRelease.bat
+++ src/Runtime/Nuget/createAllNugetRelease.bat
@@ -3,4 +3,5 @@ xsi XSPackNuget.prgx RT
 xsi XSPackNuget.prgx VO
 xsi XSPackNuget.prgx VFP
 xsi XSPackNuget.prgx XPP
-xsi XSPackNuget.prgx VOSDK
+xsi XSPackNuget.prgx VOSDK Release net46
+xsi XSPackNuget.prgx VOSDKTyped

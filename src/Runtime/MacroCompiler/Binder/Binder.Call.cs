05e31f786a83af2fa632ea80e0f49d9fca525eaf
Robert van der Hulst <robert@xsharp.eu>
2026-03-30 19:25:17 +0200

    Fix for #1860

--- src/Runtime/MacroCompiler/Binder/Binder.Call.cs
+++ src/Runtime/MacroCompiler/Binder/Binder.Call.cs
@@ -377,7 +377,10 @@ namespace XSharp.MacroCompiler
                     Convert(ref args.Args[i].Expr, FindType(parameters[i].ParameterType), conv);
                 if (conv is ConversionSymbolToConstant)
                     Convert(ref args.Args[i].Expr, FindType(parameters[i].ParameterType), BindOptions.Default);
-                HandleArgWriteBack(conv, e, ref writeBack);
+                // only write back to real locals. Do not call IVarPut() or an AssignExpr for properties
+                // or fields, or write back to auto vars or alias expressions, as they will be re-evaluated on each access and the conversion will be applied again, which is not correct for ref arguments
+                if (e.Symbol is LocalSymbol)
+                    HandleArgWriteBack(conv, e, ref writeBack);
             }
             if (ovRes.MissingArgs > 0)
             {

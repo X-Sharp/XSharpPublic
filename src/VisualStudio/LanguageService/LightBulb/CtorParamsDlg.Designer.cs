f7771d7b93401cc8b8f37dc8f624c38824c7a1ae
Robert van der Hulst <robert@xsharp.eu>
2026-05-08 14:06:23 +0200

    Revert "Convert CtorParamsDlg to WPF"
    This reverts commit 8c39e663625cbd3dac1d34331dc788289e789ee9 and fixes an issue.
    Also adds inmage to reset buttons

---
+++ src/VisualStudio/LanguageService/LightBulb/CtorParamsDlg.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace XSharp.LanguageService.Editors.LightBulb
+{ 
+    partial class CtorParamsDlg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDeselect = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.listMembers = new System.Windows.Forms.ListView();
+            this.colMembers = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(339, 303);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(56, 24);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(278, 303);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(56, 24);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDeselect);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Controls.Add(this.btnDown);
+            this.groupBox1.Controls.Add(this.btnUp);
+            this.groupBox1.Controls.Add(this.listMembers);
+            this.groupBox1.Location = new System.Drawing.Point(9, 29);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(386, 255);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pick members to be used as constructor parameters";
+            // 
+            // btnDeselect
+            // 
+            this.btnDeselect.Location = new System.Drawing.Point(303, 131);
+            this.btnDeselect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDeselect.Name = "btnDeselect";
+            this.btnDeselect.Size = new System.Drawing.Size(79, 24);
+            this.btnDeselect.TabIndex = 6;
+            this.btnDeselect.Text = "Deselect All";
+            this.btnDeselect.UseVisualStyleBackColor = true;
+            this.btnDeselect.Click += new System.EventHandler(this.btnDeselect_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(303, 102);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(79, 24);
+            this.btnSelect.TabIndex = 5;
+            this.btnSelect.Text = "Select All";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(303, 54);
+            this.btnDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(79, 24);
+            this.btnDown.TabIndex = 4;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(303, 25);
+            this.btnUp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(79, 24);
+            this.btnUp.TabIndex = 3;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // listMembers
+            // 
+            this.listMembers.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listMembers.CheckBoxes = true;
+            this.listMembers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colMembers});
+            this.listMembers.FullRowSelect = true;
+            this.listMembers.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listMembers.HideSelection = false;
+            this.listMembers.HotTracking = true;
+            this.listMembers.HoverSelection = true;
+            this.listMembers.Location = new System.Drawing.Point(14, 25);
+            this.listMembers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listMembers.Name = "listMembers";
+            this.listMembers.Size = new System.Drawing.Size(277, 216);
+            this.listMembers.TabIndex = 0;
+            this.listMembers.UseCompatibleStateImageBehavior = false;
+            this.listMembers.View = System.Windows.Forms.View.Details;
+            // 
+            // colMembers
+            // 
+            this.colMembers.Text = "Members";
+            this.colMembers.Width = 25;
+            // 
+            // CtorParamsDlg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 337);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CtorParamsDlg";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Pick members";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listMembers;
+        private System.Windows.Forms.Button btnDeselect;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.ColumnHeader colMembers;
+    }
+}
